5d9aa3688967e361e1007084665cd445c44cf50a
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2009-09-18 19:56:42 +0000

    removing dwg_var_map(...) function which is not used anymore

--- examples/test.c
+++ examples/test.c
@@ -17,38 +17,14 @@
 /*****************************************************************************/
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#include "../src/bits.h"
-#include "../src/common.h"
 #include <dwg.h>
 
 int
 test_dwg_c(char *filename);
 
-void
-test_map_R2000();
-
-int
-num_vars(enum DWG_VERSION_TYPE version);
-
 int
 main(int argc, char *argv[])
 {
-  printf("Starting to test the type-map for DWG version R2000.\n");
-  test_map_R2000();
-  printf("End of type-map test.\n");
-
-  printf("Testing number of header variables:\n");
-
-  printf("R13: %d\n", num_vars(R_13));
-  printf("R14: %d\n", num_vars(R_14));
-  printf("R2000: %d\n", num_vars(R_2000));
-  printf("R2004: %d\n", num_vars(R_2004));
-  printf("R2007: %d\n\n", num_vars(R_2007));
-
   if (argc > 1)
     return (test_dwg_c(argv[1]));
   else
@@ -68,89 +44,17 @@ test_dwg_c(char *filename)
   else
     error = dwg_read_file(FILENAME ".dwg", &dwg_struct);
 
-  if (!error)
-    {
-      //I have temporarily disabled the print module. ~Juca
-      //dwg_print(&dwg_struct);
-      /*
-       unlink ("new_result.dwg");
-       error = error || dwg_write_file ("new_result.dwg", &dwg_struct);
-       */
-    }
-
   dwg_free(&dwg_struct);
 
   if (error)
-    puts(" \"dwg.c\" ==> Error...");
+    {
+      printf("\nERROR!\n\n");
+    }
   else
     {
-      puts(" \"dwg.c\" ==> SUCCESS!");
+      printf("\nSUCCESS!\n\n");
     }
 
   return error;
 }
 
-int
-num_vars(Dwg_Version_Type version)
-{
-  int i;
-  for (i = 0; dwg_var_map(version, i) != DWG_END_OF_HEADER_VARIABLES; i++)
-    {
-    };
-  return i - 1;
-}
-
-void
-test_map_R2000()
-{
-  int i;
-  static unsigned int map_R2000[] =
-    { DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_T, DWG_DT_T, DWG_DT_T,
-        DWG_DT_T, DWG_DT_BL, DWG_DT_BL, DWG_DT_H, DWG_DT_B, DWG_DT_B, DWG_DT_B,
-        DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B,
-        DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B,
-        DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS,
-        DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS,
-        DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS,
-        DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS,
-        DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS,
-        DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD,
-        DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD,
-        DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD,
-        DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_T, DWG_DT_BL, DWG_DT_BL,
-        DWG_DT_BL, DWG_DT_BL, DWG_DT_BL, DWG_DT_BL, DWG_DT_BL, DWG_DT_BL,
-        DWG_DT_CMC, DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_H,
-        DWG_DT_BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_2RD, DWG_DT_2RD,
-        DWG_DT_BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_H, DWG_DT_H,
-        DWG_DT_BS, DWG_DT_H, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD,
-        DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_2RD,
-        DWG_DT_2RD, DWG_DT_BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_H,
-        DWG_DT_H, DWG_DT_BS, DWG_DT_H, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD,
-        DWG_DT_3BD, DWG_DT_3BD, DWG_DT_3BD, DWG_DT_T, DWG_DT_T, DWG_DT_BD,
-        DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD,
-        DWG_DT_BD, DWG_DT_BD, DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B,
-        DWG_DT_B, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BD, DWG_DT_BD,
-        DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD, DWG_DT_BD,
-        DWG_DT_BD, DWG_DT_B, DWG_DT_BS, DWG_DT_B, DWG_DT_B, DWG_DT_B, DWG_DT_B,
-        DWG_DT_CMC, DWG_DT_CMC, DWG_DT_CMC, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS,
-        DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS,
-        DWG_DT_BS, DWG_DT_BS, DWG_DT_B, DWG_DT_B, DWG_DT_BS, DWG_DT_BS,
-        DWG_DT_BS, DWG_DT_BS, DWG_DT_B, DWG_DT_BS, DWG_DT_H, DWG_DT_H,
-        DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_BS, DWG_DT_BS, DWG_DT_H, DWG_DT_H,
-        DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_H,
-        DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_BS, DWG_DT_BS, DWG_DT_T,
-        DWG_DT_T, DWG_DT_H, DWG_DT_H, DWG_DT_H, DWG_DT_BL, DWG_DT_BS,
-        DWG_DT_BS, DWG_DT_H, DWG_DT_T, DWG_DT_T, DWG_DT_H, DWG_DT_H, DWG_DT_H,
-        DWG_DT_H, DWG_DT_H, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS, DWG_DT_BS };
-
-  for (i = 0; i < 233; i++)
-    {
-      if (map_R2000[i] != dwg_var_map(R_2000, i))
-        {
-          fprintf(stderr, "map_R2000[%d]=%d\t\tdwg_var_map(R_2000, %d)=%d\n",
-              i, map_R2000[i], i, dwg_var_map(R_2000, i));
-          fprintf(stderr, "Did not pass the test.\nFailed at index=%d\n\n", i);
-          return;
-        }
-    }
-}
