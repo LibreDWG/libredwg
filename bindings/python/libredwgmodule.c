63ae17fa2f6b842f50fb269512d62481fb4b2d76
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2009-12-19 22:00:26 -0200

    yes, I know it is a bit wrong, but it is just a test. I am still trying to figure out how to return a class instance from load_file

--- bindings/python/libredwgmodule.c
+++ bindings/python/libredwgmodule.c
@@ -22,8 +22,16 @@ static PyObject *load_dwg(PyObject *self, PyObject *args)
 	return Py_BuildValue("i", 0);
 }
 
+
+static PyObject *get_version(PyObject *self, PyObject *args)
+{
+	char* version_string[]  = {"Older than R13", "R13", "R14", "R2000", "R2004", "R2007", "Newer than R2007"};
+	return Py_BuildValue("s", version_string[dwg.header.version]);
+}
+
 static PyMethodDef libredwg_methods[] = {
-	    { "load", (PyCFunction)load_dwg, METH_VARARGS, NULL },
+	    { "load", (PyCFunction)load_dwg, METH_VARARGS, "Loads a DWG file into memory." },
+	    { "get_version", (PyCFunction)get_version, METH_VARARGS, "Returns a string representing the version of the last opened DWG file." },
 	    { NULL, NULL, 0, NULL }
 };
 
