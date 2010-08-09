9c133c12b8288f9f088ed561ce3a51a7e2b96322
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2010-08-09 14:03:10 -0300

    cleaned duplicated header
    * src/bits.c

--- src/bits.c
+++ src/bits.c
@@ -19,24 +19,6 @@
  */
 
 
-/*  LibreDWG - Free DWG library                                              */
-/*                                                                           */
-/*  Copyright (C) 2009 Free Software Foundation, Inc.                        */
-/*                                                                           */
-/*  This library is free software, licensed under the terms of the GNU       */
-/*  General Public License as published by the Free Software Foundation,     */
-/*  either version 3 of the License, or (at your option) any later version.  */
-/*  You should have received a copy of the GNU General Public License        */
-/*  along with this program.  If not, see <http://www.gnu.org/licenses/>.    */
-/*****************************************************************************/
-
-/*
- * bits.c: low level read and write functions
- * written by Felipe Castro
- * modified by Felipe Corrêa da Silva Sances
- * modified by Rodrigo Rodrigues da Silva
- */
-
 #include "config.h"
 #include <stdio.h>
 #include <stdlib.h>
