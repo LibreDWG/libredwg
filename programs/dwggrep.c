c030ffa21c885b1a8f69d3901f0de8aaafc4cb9a
Reini Urban <rurban@cpan.org>
2018-06-23 17:39:35 +0200

    dwggrep: more robust probes and fallbacks
    no regex support when pcre2 is not available. (no gnu regexp, sorry)
    fix the configure probes,
    add my strcasestr fallback

--- programs/dwggrep.c
+++ programs/dwggrep.c
@@ -21,12 +21,20 @@
 #include "../src/config.h"
 #include <stdio.h>
 #include <stdlib.h>
-#undef  __DARWIN_C_LEVEL
-#define __DARWIN_C_LEVEL __DARWIN_C_FULL
-#include <string.h>
-
-#define PCRE2_CODE_UNIT_WIDTH 8
-#include <pcre2.h>
+#ifdef HAVE_STRCASESTR
+# undef  __DARWIN_C_LEVEL
+# define __DARWIN_C_LEVEL __DARWIN_C_FULL
+# include <string.h>
+#else
+# include <string.h>
+# include <ctype.h>
+#endif
+#ifdef HAVE_PCRE2_H
+# define PCRE2_CODE_UNIT_WIDTH 8
+# include <pcre2.h>
+//Maybe: for r2007+ use pcre2-16, before use pcre2-8
+//       Currently we convert to UTF-8
+#endif
 
 #include "dwg.h"
 #include "../src/logging.h"
@@ -38,6 +46,12 @@ int verbosity(int argc, char **argv, int i, unsigned int *opts);
 #include "common.inc"
 #include "dwg_api.h"
 
+#ifndef HAVE_PCRE2_H
+# define PCRE2_DUPNAMES 0
+# define PCRE2_CASELESS 1
+# define PCRE2_EXTENDED 2
+#endif
+
 char *pattern;
 char buf[4096];
 int options = PCRE2_DUPNAMES;
@@ -46,19 +60,21 @@ int opt_count = 0;
 /* the current version per spec block */
 static unsigned int cur_ver = 0;
 
-#undef USE_MATCH_CONTEXT
+#ifdef HAVE_PCRE2_H
+# undef USE_MATCH_CONTEXT
 /* pcre2_compile */
 static pcre2_code_8 *ri;
 static pcre2_match_data_8 *match_data;
 static pcre2_match_context_8 *match_context = NULL;
 
-#ifdef USE_MATCH_CONTEXT
+# ifdef USE_MATCH_CONTEXT
 static pcre2_jit_stack *jit_stack = NULL;
 static pcre2_compile_context_8 *compile_context = NULL;
-#endif
-#define PUBLIC_JIT_MATCH_OPTIONS \
+# endif
+# define PUBLIC_JIT_MATCH_OPTIONS \
    (PCRE2_NO_UTF_CHECK|PCRE2_NOTBOL|PCRE2_NOTEOL|PCRE2_NOTEMPTY|\
     PCRE2_NOTEMPTY_ATSTART|PCRE2_PARTIAL_SOFT|PCRE2_PARTIAL_HARD)
+#endif
 
 static int usage(void) {
   printf("\nUsage: dwggrep [-cRr] pattern *.dwg\n");
@@ -70,10 +86,15 @@ static int opt_version(void) {
 }
 static int help(void) {
   printf("\nUsage: dwggrep [OPTIONS]... pattern files\n");
-  printf("Search regex pattern in a list of DWGs.\n"
-         "\n");
+#ifdef HAVE_PCRE2_H
+  printf("Search regex pattern in a list of DWGs.\n\n");
+#else
+  printf("Search string (no regex) in a list of DWGs.\n\n");
+#endif
   printf("  -i                        Case-insensitive pattern\n");
+#ifdef HAVE_PCRE2_H
   printf("  -x                        Extended regex pattern\n");
+#endif
 #if 0
   printf("  --type NAME               Search only NAME entities or objects.\n");
   printf("  --dxf NUM                 Search only DXF group NUM fields.\n");
@@ -93,22 +114,53 @@ static int help(void) {
 static int
 do_match (char *filename, char *entity, int dxf, char* text, int textlen)
 {
+#ifdef HAVE_PCRE2_H
   int found = pcre2_jit_match_8(ri, (PCRE2_SPTR8)text, textlen, 0,
                                 PUBLIC_JIT_MATCH_OPTIONS,
-                                match_data, /* block for storing the result */
-                                match_context);
-  if (found > 0) {
+                                match_data,     /* block for storing the result */
+                                match_context); /* disabled */
+  if (found >= 0) {
     if (!opt_count)
       printf("%s %s %d: %s\n", filename, entity, dxf, text);
     return 1;
   }
-  else if (options & PCRE2_CASELESS)
+#else
+  if (options & PCRE2_CASELESS)
     {
-      if (strcasestr(text, pattern)) {
-        if (!opt_count)
-          printf("%s %s %d: %s\n", filename, entity, dxf, text);
-        return 1;
-      }
+# ifndef HAVE_STRCASESTR
+      int i, len;
+      char *dest = text;
+      int dlen = textlen;
+      char *src = pattern;
+      int slen = strlen(pattern);
+      while (*dest && dlen)
+        {
+          i = 0;
+          len = slen;
+          dlen = textlen;
+          if (toupper((unsigned char)dest[i]) != toupper((unsigned char)src[i])) {
+            break;
+          }
+          /* move to the next char */
+          i++;
+          len--;
+          dlen--;
+
+          if (src[i] == '\0' || !len) {
+            if (!opt_count)
+              printf("%s %s %d: %s\n", filename, entity, dxf, text);
+            return 1;
+          }
+          dest++;
+        }
+# else
+      if (strcasestr(text, pattern))
+        {
+          if (!opt_count)
+            printf("%s %s %d: %s\n", filename, entity, dxf, text);
+          return 1;
+        }
+# endif
     }
   else
     {
@@ -118,6 +170,7 @@ do_match (char *filename, char *entity, int dxf, char* text, int textlen)
         return 1;
       }
     }
+#endif
   return 0;
 }
   
@@ -207,9 +260,12 @@ main (int argc, char *argv[])
   short numtype = 0;
   int plen;
   int errcode;
+#ifdef HAVE_PCRE2_H
   PCRE2_SIZE erroffset;
   /* pcre_compile */
-  int have_jit, found = 0;
+  int have_jit;
+#endif
+  int count = 0;
 
   // check args
   if (argc < 2)
@@ -221,11 +277,13 @@ main (int argc, char *argv[])
       options |= PCRE2_CASELESS;
       i++;
     }
+#ifdef HAVE_PCRE2_H
   if (i < argc && !strcmp(argv[i], "-x"))
     {
       options |= PCRE2_EXTENDED;
       i++;
     }
+#endif
   if (i < argc-1 && !strcmp(argv[i], "--type"))
     {
       if (numtype>=10) exit(1);
@@ -252,21 +310,22 @@ main (int argc, char *argv[])
     return usage();
 
   pattern = argv[i]; plen = strlen(pattern);
+#ifdef HAVE_PCRE2_H
   pcre2_config_8(PCRE2_CONFIG_JIT, &have_jit);
-
   ri = pcre2_compile_8(
      (PCRE2_SPTR8)pattern, plen, /* pattern */
      options,      /* options */
      &errcode,     /* errors */
      &erroffset,   /* error offset */
-#ifdef USE_MATCH_CONTEXT
+# ifdef USE_MATCH_CONTEXT
      compile_context
-#else
+# else
      NULL
-#endif
+# endif
     );
   match_data = pcre2_match_data_create_from_pattern_8(ri, NULL);
   pcre2_jit_compile_8(ri, PCRE2_JIT_COMPLETE); /* no partial matches */
+#endif
 
   //for all filenames...
   for (j=i+1; j<argc; j++)
@@ -284,19 +343,18 @@ main (int argc, char *argv[])
           continue;
         }
 
-      //TODO: for r2007+ use pcre2-16, before use pcre2-8
       for (k=0; k < dwg.block_control.num_entries; k++)
         {
-          found += match_BLOCK_HEADER(filename, dwg.block_control.block_headers[k]);
+          count += match_BLOCK_HEADER(filename, dwg.block_control.block_headers[k]);
         }
-      found += match_BLOCK_HEADER(filename, dwg.block_control.model_space);
-      found += match_BLOCK_HEADER(filename, dwg.block_control.paper_space);
+      count += match_BLOCK_HEADER(filename, dwg.block_control.model_space);
+      count += match_BLOCK_HEADER(filename, dwg.block_control.paper_space);
 
       if (j < argc)
         dwg_free(&dwg); //skip the last free
     }
   if (opt_count)
-    printf("%d\n", found);
+    printf("%d\n", count);
 
-  return found ? 0 : 1;
+  return count ? 0 : 1;
 }
