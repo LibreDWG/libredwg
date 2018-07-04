4d7bb39a51345f6b5a5b3936d02d37ba8c7b8d01
Reini Urban <rurban@cpan.org>
2018-07-04 09:55:47 +0200

    add git-version-gen generated version
    from git tags. but we dont use a v prefix.

--- autogen.sh
+++ autogen.sh
@@ -10,7 +10,10 @@
 # - automake (GNU automake) 1.11.1
 # - ltmain.sh (GNU libtool) 2.2.6b
 
-set -ex
+set -x
+if test -d .git; then git fetch --tags; test -f .tarball-version && rm .tarball-version; fi
+test -f .version || git describe --long --tags --always > .version
+set -e
 autoreconf --install --symlink "$@" -I m4
 
 # autogen.sh ends here
