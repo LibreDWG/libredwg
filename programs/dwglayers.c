1a6265ed4d4a9a6a3c92927538502a9044bc9295
Reini Urban <rurban@cpan.org>
2018-07-24 22:26:50 +0200

    dwglayers: ignore DICTIONARY
    as layer_control member. This has no entry_name

--- programs/dwglayers.c
+++ programs/dwglayers.c
@@ -129,6 +129,9 @@ main (int argc, char *argv[])
 
   for (i=0; i < dwg.layer_control.num_entries; i++)
     {
+      Dwg_Object *obj = dwg.layer_control.layers[i]->obj;
+      if (obj->type != DWG_TYPE_LAYER)
+        break;
       layer = dwg.layer_control.layers[i]->obj->tio.object->tio.LAYER;
       layer->on = layer->color.index > 0;
       if (on && (!layer->on || layer->frozen))
