a285a54d265d066b802069126dcbf4b838c7495a
Reini Urban <rurban@cpan.org>
2018-11-23 22:02:02 +0100

    examples: improve load_dwg.py
    dont fail on non-critical errors
    dont import twice

--- examples/load_dwg.py
+++ examples/load_dwg.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import libredwg
+#import libredwg
 from libredwg import *
 
 import sys
@@ -14,14 +14,15 @@ a = Dwg_Data()
 a.object = new_Dwg_Object_Array(1000)
 error = dwg_read_file(filename, a)
 
-if (error != 0):
-    print "Error!"
-    exit()
+if (error > 0): # critical errors
+    print "Error: ", error
+    if (error > 127):
+        exit()
 
 print ".dwg version: %s" % a.header.version
 print "Num objects: %d " % a.num_objects
 
-#XXX TODO segfault when reading the LAYER_CONTROL in python (not in C)
+#XXX TODO Error: Dwg_Object_LAYER_CONTROL object has no attribute 'tio'
 #print "Num layers: %d" % a.layer_control.tio.object.tio.LAYER_CONTROL.num_entries
 
 #XXX ugly, but works
