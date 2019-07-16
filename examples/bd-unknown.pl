22850bdaed03fd33adbf32b6a7fcb405a81a592f
Reini Urban <rurban@cpan.org>
2019-07-16 15:57:51 +0200

    fix bd-unknown.pl to be run from examples
    via make. add some new

--- examples/bd-unknown.pl
+++ examples/bd-unknown.pl
@@ -2,10 +2,9 @@
 # get all double values which are not found, and check for hints
 # for a different binary repr.
 # update bd-unknown.inc then
-# usage: examples/bd-unknown.pl|sort|uniq > examples/bd-unknown.inc
+# usage: bd-unknown.pl|sort|uniq >bd-unknown.inc
 
-for my $fn (<examples/*.pi>) {
-  my $log = "$fn.log";
+for my $fn (<*.pi>) {
   my $o = $fn;
   $o =~ s{examples/}{};
   $o =~ s{\.pi}{};
