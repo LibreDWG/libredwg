17abab56d170c92c271f3f97c46680e51eb88c1f
Reini Urban <rurban@cpan.org>
2019-07-24 19:24:05 +0200

    unit-testing: dimension

--- test/unit-testing/dim_ang2ln.c
+++ test/unit-testing/dim_ang2ln.c
@@ -2,193 +2,68 @@
 #include "common.c"
 
 void
-low_level_process (dwg_object *obj)
-{
-  dwg_ent_dim_ang2ln *dim_ang2ln = dwg_object_to_DIMENSION_ANG2LN (obj);
-
-  printf ("horiz dir of dim_ang2ln : %f\n", dim_ang2ln->horiz_dir);
-  printf ("lspace factor of dim_ang2ln : %f\n", dim_ang2ln->lspace_factor);
-  printf ("lspace style of dim_ang2ln : " FORMAT_BS "\n",
-          dim_ang2ln->lspace_style);
-  printf ("attachment of dim_ang2ln : " FORMAT_BS "\n",
-          dim_ang2ln->attachment);
-  printf ("elevation of dim_ang2ln : %f\n", dim_ang2ln->elevation);
-  printf ("extrusion of dim_ang2ln : x = %f, y = %f, z = %f\n",
-          dim_ang2ln->extrusion.x, dim_ang2ln->extrusion.y,
-          dim_ang2ln->extrusion.z);
-  printf ("ins_scale of dim_ang2ln : x = %f, y = %f, z = %f\n",
-          dim_ang2ln->ins_scale.x, dim_ang2ln->ins_scale.y,
-          dim_ang2ln->ins_scale.z);
-  printf ("pt10 of dim_ang2ln : x = %f, y = %f, z = %f\n",
-          dim_ang2ln->def_pt.x, dim_ang2ln->def_pt.y, dim_ang2ln->def_pt.z);
-  printf ("pt13 of dim_ang2ln : x = %f, y = %f, z = %f\n",
-          dim_ang2ln->_13_pt.x, dim_ang2ln->_13_pt.y, dim_ang2ln->_13_pt.z);
-  printf ("pt14 of dim_ang2ln : x = %f, y = %f, z = %f\n",
-          dim_ang2ln->_14_pt.x, dim_ang2ln->_14_pt.y, dim_ang2ln->_14_pt.z);
-  printf ("pt15 of dim_ang2ln : x = %f, y = %f, z = %f\n",
-          dim_ang2ln->first_arc_pt.x, dim_ang2ln->first_arc_pt.y,
-          dim_ang2ln->first_arc_pt.z);
-  printf ("pt12 of dim_ang2ln : x = %f, y = %f\n", dim_ang2ln->clone_ins_pt.x,
-          dim_ang2ln->clone_ins_pt.y);
-  printf ("pt16 of dim_ang2ln : x = %f, y = %f\n", dim_ang2ln->_16_pt.x,
-          dim_ang2ln->_16_pt.y);
-  printf ("text_mid_pt of dim_ang2ln : x = %f, y = %f\n",
-          dim_ang2ln->text_midpt.x, dim_ang2ln->text_midpt.y);
-  printf ("user text of dim_ang2ln : %s\n", dim_ang2ln->user_text);
-  printf ("text rotation of dim_ang2ln : %f\n", dim_ang2ln->text_rotation);
-  printf ("ins rotation of dim_ang2ln : %f\n", dim_ang2ln->ins_rotation);
-  printf ("arrow1 of dim_ang2ln : " FORMAT_B "\n", dim_ang2ln->flip_arrow1);
-  printf ("arrow2 of dim_ang2ln : " FORMAT_B "\n", dim_ang2ln->flip_arrow2);
-  printf ("flags1 of dim_ang2ln : " FORMAT_RC "\n", dim_ang2ln->flag1);
-  printf ("act_measurement of dim_ang2ln : %f\n", dim_ang2ln->act_measurement);
-}
-
-void
 api_process (dwg_object *obj)
 {
   int error;
-  double elevation, act_measure, horiz_dir, lspace_factor, text_rot, ins_rot,
-      ext_line_rot, dim_rot;
+  double elevation, act_measurement, horiz_dir, lspace_factor,
+    text_rotation, ins_rotation;
   BITCODE_B flip_arrow1, flip_arrow2;
-  BITCODE_RC flags1;
+  BITCODE_RC class_version, flag, flag1;
   BITCODE_BS lspace_style, attachment;
   char *user_text;
-  dwg_point_2d text_mid_pt, pt12, pt16;
-  dwg_point_3d pt10, pt13, pt14, pt15, ext, ins_scale;
+  dwg_point_2d text_midpt, pt12, pt16, pt2d;
+  dwg_point_3d pt10, pt13, pt14, first_arc_pt, ext, ins_scale, pt3d;
+  BITCODE_H dimstyle, block;
 
   dwg_ent_dim_ang2ln *dim_ang2ln = dwg_object_to_DIMENSION_ANG2LN (obj);
   dwg_ent_dim *dim = dwg_object_to_DIMENSION (obj);
 
-  horiz_dir = dwg_ent_dim_get_horiz_dir (dim, &error);
-  if (!error)
-    printf ("horiz dir of dim : %f\n", horiz_dir);
-  else
-    printf ("error in reading horiz dir \n");
+  /* common */
+  CHK_ENTITY_TYPE (dim, DIMENSION_ANG2LN, class_version, RC, class_version);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, lspace_style, BS, lspace_style);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, lspace_factor, BD, lspace_factor);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, act_measurement,
+                         BD, act_measurement);
+  CHK_ENTITY_TYPE (dim, DIMENSION_ANG2LN, attachment, BS, attachment);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, elevation, BD, elevation);
+  CHK_ENTITY_3RD (dim, DIMENSION_ANG2LN, extrusion, ext);
+  CHK_ENTITY_2RD_W_OLD (dim, DIMENSION_ANG2LN, clone_ins_pt, pt12);
+  CHK_ENTITY_2RD_W_OLD (dim, DIMENSION_ANG2LN, text_midpt, text_midpt);
+  CHK_ENTITY_UTF8TEXT (dim, DIMENSION_ANG2LN, user_text, user_text);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, text_rotation, BD, text_rotation);
+  CHK_ENTITY_3RD_W_OLD (dim, DIMENSION_ANG2LN, ins_scale, ins_scale);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, ins_rotation, BD, ins_rotation);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, horiz_dir, BD, horiz_dir);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, flip_arrow1, B, flip_arrow1);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, flip_arrow2, B, flip_arrow2);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ANG2LN, flag1, RC, flag1);
+  CHK_ENTITY_TYPE (dim, DIMENSION_ANG2LN, flag, RC, flag);
 
-  lspace_factor = dwg_ent_dim_get_lspace_factor (dim, &error);
-  if (!error)
-    printf ("lspace factor of dim : %f\n", lspace_factor);
-  else
-    printf ("error in reading lspace factor \n");
+  /* ang2ln */
+  CHK_ENTITY_3RD_W_OLD (dim_ang2ln, DIMENSION_ANG2LN, def_pt, pt10);
 
-  lspace_style = dwg_ent_dim_get_lspace_style (dim, &error);
-  if (!error)
-    printf ("lspace style of dim : " FORMAT_BS "\n", lspace_style);
+  CHK_ENTITY_3RD (dim_ang2ln, DIMENSION_ANG2LN, _13_pt, pt13);
+  dwg_ent_dim_ang2ln_get_13_pt (dim_ang2ln, &pt3d, &error);
+  if (error || memcmp (&pt13, &pt3d, sizeof (pt13)))
+    fail ("old API dwg_ent_dim_ang2ln_get_13_pt");
   else
-    printf ("error in reading lspace style \n");
+    pass ();
+  CHK_ENTITY_3RD (dim_ang2ln, DIMENSION_ANG2LN, _14_pt, pt14);
+  dwg_ent_dim_ang2ln_get_14_pt (dim_ang2ln, &pt3d, &error);
+  if (error || memcmp (&pt14, &pt3d, sizeof (pt14)))
+    fail ("old API dwg_ent_dim_ang2ln_get_14_pt");
+  else
+    pass ();
+  CHK_ENTITY_2RD (dim_ang2ln, DIMENSION_ANG2LN, _16_pt, pt16);
+  dwg_ent_dim_ang2ln_get_16_pt (dim_ang2ln, &pt2d, &error);
+  if (error || memcmp (&pt16, &pt2d, sizeof (pt16)))
+    fail ("old API dwg_ent_dim_ang2ln_get_16_pt");
+  else
+    pass ();
 
-  attachment = dwg_ent_dim_get_attachment (dim, &error);
-  if (!error)
-    printf ("attachment of dim : " FORMAT_BS "\n", attachment);
-  else
-    printf ("error in reading attachment \n");
+  CHK_ENTITY_3RD_W_OLD (dim_ang2ln, DIMENSION_ANG2LN, first_arc_pt,
+                        first_arc_pt);
 
-  elevation = dwg_ent_dim_get_elevation (dim, &error);
-  if (!error)
-    printf ("Radius of dim : %f\n", elevation);
-  else
-    printf ("error in reading elevation \n");
-
-  dwg_ent_dim_get_extrusion (dim, &ext, &error);
-  if (!error)
-    printf ("extrusion of dim : x = %f, y = %f, z = %f\n", ext.x, ext.y,
-            ext.z);
-  else
-    printf ("error in reading extrusion \n");
-
-  dwg_ent_dim_get_ins_scale (dim, &ins_scale, &error);
-  if (!error)
-    printf ("ins_scale of dim : x = %f, y = %f, z = %f\n", ins_scale.x,
-            ins_scale.y, ins_scale.z);
-  else
-    printf ("error in reading ins_scale \n");
-
-  dwg_ent_dim_ang2ln_get_def_pt (dim_ang2ln, &pt10, &error);
-  if (!error)
-    printf ("pt10 of dim_ang2ln : x = %f, y = %f, z = %f\n", pt10.x, pt10.y,
-            pt10.z);
-  else
-    printf ("error in reading pt10 \n");
-
-  dwg_ent_dim_ang2ln_get_13_pt (dim_ang2ln, &pt13, &error);
-  if (!error)
-    printf ("pt13 of dim_ang2ln : x = %f, y = %f, z = %f\n", pt13.x, pt13.y,
-            pt13.z);
-  else
-    printf ("error in reading pt13 \n");
-
-  dwg_ent_dim_ang2ln_get_14_pt (dim_ang2ln, &pt14, &error);
-  if (!error)
-    printf ("pt14 of dim_ang2ln : x = %f, y = %f, z = %f\n", pt14.x, pt14.y,
-            pt14.z);
-  else
-    printf ("error in reading pt14 \n");
-
-  dwg_ent_dim_ang2ln_get_first_arc_pt (dim_ang2ln, &pt15, &error);
-  if (!error)
-    printf ("pt15 of dim_ang2ln : x = %f, y = %f, z = %f\n", pt15.x, pt15.y,
-            pt15.z);
-  else
-    printf ("error in reading pt14 \n");
-
-  dwg_ent_dim_get_clone_ins_pt (dim, &pt12, &error);
-  if (!error)
-    printf ("pt12 of dim : x = %f, y = %f\n", pt12.x, pt12.y);
-  else
-    printf ("error in reading pt12 \n");
-
-  dwg_ent_dim_ang2ln_get_16_pt (dim_ang2ln, &pt16, &error);
-  if (!error)
-    printf ("pt16 of dim_ang2ln : x = %f, y = %f\n", pt16.x, pt16.y);
-  else
-    printf ("error in reading pt16 \n");
-
-  dwg_ent_dim_get_text_midpt (dim, &text_mid_pt, &error);
-  if (!error)
-    printf ("text_mid_pt of dim : x = %f, y = %f\n", text_mid_pt.x,
-            text_mid_pt.y);
-  else
-    printf ("error in reading text_mid_pt \n");
-
-  user_text = dwg_ent_dim_get_user_text (dim, &error);
-  if (!error)
-    printf ("user text of dim : %s\n", user_text);
-  else
-    printf ("error in reading user_text \n");
-
-  text_rot = dwg_ent_dim_get_text_rotation (dim, &error);
-  if (!error)
-    printf (" text rotation of dim : %f\n", text_rot);
-  else
-    printf ("error in reading text rotation \n");
-
-  ins_rot = dwg_ent_dim_get_ins_rotation (dim, &error);
-  if (!error)
-    printf ("ins rotation of dim : %f\n", ins_rot);
-  else
-    printf ("error in reading ins rotation \n");
-
-  flip_arrow1 = dwg_ent_dim_get_flip_arrow1 (dim, &error);
-  if (!error)
-    printf ("arrow1 of dim : " FORMAT_B "\n", flip_arrow1);
-  else
-    printf ("error in reading arrow1 \n");
-
-  flip_arrow2 = dwg_ent_dim_get_flip_arrow2 (dim, &error);
-  if (!error)
-    printf ("arrow2 of dim : " FORMAT_B "\n", flip_arrow2);
-  else
-    printf ("error in reading arrow2 \n");
-
-  flags1 = dwg_ent_dim_get_flag1 (dim, &error);
-  if (!error)
-    printf ("flags1 of dim : " FORMAT_RC "\n", flags1);
-  else
-    printf ("error in reading flags1 \n");
-
-  act_measure = dwg_ent_dim_get_act_measurement (dim, &error);
-  if (!error)
-    printf ("act_measurement of dim : %f\n", act_measure);
-  else
-    printf ("error in reading act_measurement \n");
+  CHK_ENTITY_H (dim_ang2ln, DIMENSION_ANG2LN, dimstyle, dimstyle);
+  CHK_ENTITY_H (dim_ang2ln, DIMENSION_ANG2LN, block, block);
 }
--- test/unit-testing/dim_ordinate.c
+++ test/unit-testing/dim_ordinate.c
@@ -2,188 +2,49 @@
 #include "common.c"
 
 void
-low_level_process (dwg_object *obj)
-{
-  dwg_ent_dim_ordinate *dim_ordinate = dwg_object_to_DIMENSION_ORDINATE (obj);
-
-  printf ("horiz dir of dim_ordinate : %f\n", dim_ordinate->horiz_dir);
-  printf ("lspace factor of dim_ordinate : %f\n", dim_ordinate->lspace_factor);
-  printf ("lspace style of dim_ordinate : " FORMAT_BS "\n",
-          dim_ordinate->lspace_style);
-  printf ("attach point of dim_ordinate : " FORMAT_BS "\n",
-          dim_ordinate->attachment);
-  printf ("elevation of dim_ordinate : %f\n", dim_ordinate->elevation);
-  printf ("extrusion of dim_ordinate : x = %f, y = %f, z = %f\n",
-          dim_ordinate->extrusion.x, dim_ordinate->extrusion.y,
-          dim_ordinate->extrusion.z);
-  printf ("ins_scale of dim_ordinate : x = %f, y = %f, z = %f\n",
-          dim_ordinate->ins_scale.x, dim_ordinate->ins_scale.y,
-          dim_ordinate->ins_scale.z);
-  printf ("def_pt of dim_ordinate : x = %f, y = %f, z = %f\n",
-          dim_ordinate->def_pt.x, dim_ordinate->def_pt.y,
-          dim_ordinate->def_pt.z);
-  printf ("feature_location_pt of dim_ordinate : x = %f, y = %f, z = %f\n",
-          dim_ordinate->feature_location_pt.x,
-          dim_ordinate->feature_location_pt.y,
-          dim_ordinate->feature_location_pt.z);
-  printf ("leader_endpt of dim_ordinate : x = %f, y = %f, z = %f\n",
-          dim_ordinate->leader_endpt.x, dim_ordinate->leader_endpt.y,
-          dim_ordinate->leader_endpt.z);
-  printf ("clone_ins_pt of dim_ordinate : x = %f, y = %f\n",
-          dim_ordinate->clone_ins_pt.x, dim_ordinate->clone_ins_pt.y);
-  printf ("text_mid_pt of dim_ordinate : x = %f, y = %f\n",
-          dim_ordinate->text_midpt.x, dim_ordinate->text_midpt.y);
-  printf ("user text of dim_ordinate : %s\n", dim_ordinate->user_text);
-  printf ("text rotation of dim_ordinate : %f\n", dim_ordinate->text_rotation);
-  printf ("ins rotation of dim_ordinate : %f\n", dim_ordinate->ins_rotation);
-  printf ("arrow1 of dim_ordinate : " FORMAT_B "\n",
-          dim_ordinate->flip_arrow1);
-  printf ("arrow2 of dim_ordinate : " FORMAT_B "\n",
-          dim_ordinate->flip_arrow2);
-  printf ("flags1 of dim_ordinate : " FORMAT_RC "\n", dim_ordinate->flag1);
-  printf ("flags2 of dim_ordinate : " FORMAT_RC "\n", dim_ordinate->flag2);
-  printf ("act_measurement of dim_ordinate : %f\n",
-          dim_ordinate->act_measurement);
-}
-
-void
 api_process (dwg_object *obj)
 {
   int error;
-  double elevation, act_measure, horiz_dir, lspace_factor, text_rot, ins_rot;
+  double elevation, act_measurement, horiz_dir, lspace_factor,
+    text_rotation, ins_rotation, ext_line_rotation;
   BITCODE_B flip_arrow1, flip_arrow2;
-  BITCODE_RC flag1, flag2;
+  BITCODE_RC class_version, flag, flag1, flag2;
   BITCODE_BS lspace_style, attachment;
   char *user_text;
-  dwg_point_2d text_mid_pt, pt12;
-  dwg_point_3d pt10, pt13, pt14, ext, ins_scale;
+  dwg_point_2d text_midpt, pt12;
+  dwg_point_3d pt10, pt13, pt14, ext, ins_scale, pt3d;
+  BITCODE_H dimstyle, block;
 
   dwg_ent_dim_ordinate *dim_ordinate = dwg_object_to_DIMENSION_ORDINATE (obj);
   dwg_ent_dim *dim = dwg_object_to_DIMENSION (obj);
 
-  horiz_dir = dwg_ent_dim_get_horiz_dir (dim, &error);
-  if (!error)
-    printf ("horiz dir of dim_ordinate : %f\n", horiz_dir);
-  else
-    printf ("error in reading horiz dir \n");
+  /* common */
+  CHK_ENTITY_TYPE (dim, DIMENSION_ORDINATE, class_version, RC, class_version);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, lspace_style, BS, lspace_style);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, lspace_factor, BD, lspace_factor);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, act_measurement,
+                         BD, act_measurement);
+  CHK_ENTITY_TYPE (dim, DIMENSION_ORDINATE, attachment, BS, attachment);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, elevation, BD, elevation);
+  CHK_ENTITY_3RD (dim, DIMENSION_ORDINATE, extrusion, ext);
+  CHK_ENTITY_2RD_W_OLD (dim, DIMENSION_ORDINATE, clone_ins_pt, pt12);
+  CHK_ENTITY_2RD_W_OLD (dim, DIMENSION_ORDINATE, text_midpt, text_midpt);
+  CHK_ENTITY_UTF8TEXT (dim, DIMENSION_ORDINATE, user_text, user_text);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, text_rotation, BD, text_rotation);
+  CHK_ENTITY_3RD_W_OLD (dim, DIMENSION_ORDINATE, ins_scale, ins_scale);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, ins_rotation, BD, ins_rotation);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, horiz_dir, BD, horiz_dir);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, flip_arrow1, B, flip_arrow1);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, flip_arrow2, B, flip_arrow2);
+  CHK_ENTITY_TYPE_W_OLD (dim, DIMENSION_ORDINATE, flag1, RC, flag1);
+  CHK_ENTITY_TYPE (dim, DIMENSION_ORDINATE, flag, RC, flag);
 
-  lspace_factor = dwg_ent_dim_get_lspace_factor (dim, &error);
-  if (!error)
-    printf ("lspace factor of dim_ordinate : %f\n", lspace_factor);
-  else
-    printf ("error in reading lspace factor \n");
+  /* ordinate */
+  CHK_ENTITY_3RD_W_OLD (dim_ordinate, DIMENSION_ORDINATE, def_pt, pt10);
+  CHK_ENTITY_3RD_W_OLD (dim_ordinate, DIMENSION_ORDINATE, feature_location_pt, pt13);
+  CHK_ENTITY_3RD_W_OLD (dim_ordinate, DIMENSION_ORDINATE, leader_endpt, pt14);
+  CHK_ENTITY_TYPE_W_OLD (dim_ordinate, DIMENSION_ORDINATE, flag2, RC, flag2);
 
-  lspace_style = dwg_ent_dim_get_elevation (dim, &error);
-  if (!error)
-    printf ("lspace style of dim_ordinate : " FORMAT_BS "\n", lspace_style);
-  else
-    printf ("error in reading lspace style \n");
-
-  attachment = dwg_ent_dim_get_attachment (dim, &error);
-  if (!error)
-    printf ("attachment of dim_ordinate : " FORMAT_BS "\n", attachment);
-  else
-    printf ("error in reading attachment \n");
-
-  elevation = dwg_ent_dim_get_elevation (dim, &error);
-  if (!error)
-    printf ("elevation of dim_ordinate : %f\n", elevation);
-  else
-    printf ("error in reading elevation \n");
-
-  dwg_ent_dim_get_extrusion (dim, &ext, &error);
-  if (!error)
-    printf ("extrusion of dim_ordinate : x = %f, y = %f, z = %f\n", ext.x,
-            ext.y, ext.z);
-  else
-    printf ("error in reading extrusion \n");
-
-  dwg_ent_dim_get_ins_scale (dim, &ins_scale, &error);
-  if (!error)
-    printf ("ins_scale of dim_ordinate : x = %f, y = %f, z = %f\n",
-            ins_scale.x, ins_scale.y, ins_scale.z);
-  else
-    printf ("error in reading ins_scale \n");
-
-  dwg_ent_dim_ordinate_get_def_pt (dim_ordinate, &pt10, &error);
-  if (!error)
-    printf ("def_pt of dim_ordinate : x = %f, y = %f, z = %f\n", pt10.x,
-            pt10.y, pt10.z);
-  else
-    printf ("error in reading pt10 \n");
-
-  dwg_ent_dim_ordinate_get_feature_location_pt (dim_ordinate, &pt13, &error);
-  if (!error)
-    printf ("feature_location_pt of dim_ordinate : x = %f, y = %f, z = %f\n",
-            pt13.x, pt13.y, pt13.z);
-  else
-    printf ("error in reading feature_location_pt\n");
-
-  dwg_ent_dim_ordinate_get_leader_endpt (dim_ordinate, &pt14, &error);
-  if (!error)
-    printf ("leader_endpt of dim_ordinate : x = %f, y = %f, z = %f\n", pt14.x,
-            pt14.y, pt14.z);
-  else
-    printf ("error in reading leader_endpt\n");
-
-  dwg_ent_dim_get_clone_ins_pt (dim, &pt12, &error);
-  if (!error)
-    printf ("clone_ins_pt of dim_ordinate : x = %f, y = %f\n", pt12.x, pt12.y);
-  else
-    printf ("error in reading clone_ins_pt\n");
-
-  dwg_ent_dim_get_text_midpt (dim, &text_mid_pt, &error);
-  if (!error)
-    printf ("text_mid_pt of dim_ordinate : x = %f, y = %f\n", text_mid_pt.x,
-            text_mid_pt.y);
-  else
-    printf ("error in reading text_mid_pt \n");
-
-  user_text = dwg_ent_dim_get_user_text (dim, &error);
-  if (!error)
-    printf ("user text of dim_ordinate : %s\n", user_text);
-  else
-    printf ("error in reading user_text \n");
-
-  text_rot = dwg_ent_dim_get_text_rotation (dim, &error);
-  if (!error)
-    printf (" text rotation of dim_ordinate : %f\n", text_rot);
-  else
-    printf ("in reading text rotation \n");
-
-  ins_rot = dwg_ent_dim_get_ins_rotation (dim, &error);
-  if (!error)
-    printf ("ins rotation of dim_ordinate : %f\n", ins_rot);
-  else
-    printf ("in reading ins rotation \n");
-
-  flip_arrow1 = dwg_ent_dim_get_flip_arrow1 (dim, &error);
-  if (!error)
-    printf ("arrow1 of dim_ordinate : " FORMAT_B "\n", flip_arrow1);
-  else
-    printf ("in reading arrow1 \n");
-
-  flip_arrow2 = dwg_ent_dim_get_flip_arrow2 (dim, &error);
-  if (!error)
-    printf ("arrow1 of dim_ordinate : " FORMAT_B "\n", flip_arrow2);
-  else
-    printf ("in reading arrow1 \n");
-
-  flag1 = dwg_ent_dim_get_flag1 (dim, &error);
-  if (!error)
-    printf ("flag1 of dim_ordinate : " FORMAT_RC "\n", flag1);
-  else
-    printf ("in reading flag1 \n");
-
-  flag2 = dwg_ent_dim_ordinate_get_flag2 (dim_ordinate, &error);
-  if (!error)
-    printf ("flag2 of dim_ordinate : " FORMAT_RC "\n", flag2);
-  else
-    printf ("in reading flag2 \n");
-
-  act_measure = dwg_ent_dim_get_act_measurement (dim, &error);
-  if (!error)
-    printf ("act_measurement of dim_ordinate : %f\n", act_measure);
-  else
-    printf ("in reading act_measurement \n");
+  CHK_ENTITY_H (dim_ordinate, DIMENSION_ORDINATE, dimstyle, dimstyle);
+  CHK_ENTITY_H (dim_ordinate, DIMENSION_ORDINATE, block, block);
 }
