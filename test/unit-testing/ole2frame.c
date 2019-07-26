d4f097164e09a273500cddcca0f1812bd6b965ec
Reini Urban <rurban@cpan.org>
2019-07-26 10:02:45 +0200

    ole2frame: data != dat_length mismatch TODO

--- test/unit-testing/ole2frame.c
+++ test/unit-testing/ole2frame.c
@@ -17,11 +17,18 @@ api_process (dwg_object *obj)
   CHK_ENTITY_TYPE_W_OLD (ole2frame, OLE2FRAME, data_length, BS, data_length);
 
   //CHK_ENTITY_TYPE_W_OLD (ole2frame, OLE2FRAME, data, RC*, data);
-  if (!dwg_dynapi_entity_value (ole2frame, "OLE2FRAME", "data", &data, NULL))
-    fail ("OLE2FRAME.data");
-  data1 = dwg_ent_ole2frame_get_data (ole2frame, &error);
-  if (error || memcmp (&data, &data1, data_length))
-    ok ("TODO: dwg_ent_ole2frame_get_data error: %d", error);
+  if (data_length != dwg_ent_ole2frame_get_data_length (ole2frame, &error)
+      || error)
+    ok ("TODO: dwg_ent_ole2frame_get_data_length error: %d", error);
+  else
+    {
+      if (!dwg_dynapi_entity_value (ole2frame, "OLE2FRAME", "data", &data, NULL))
+        fail ("OLE2FRAME.data");
+      data1 = dwg_ent_ole2frame_get_data (ole2frame, &error);
+      // TODO wrong length
+      if (error /* || memcmp (&data, &data1, data_length) */)
+        ok ("TODO: dwg_ent_ole2frame_get_data error: %d", error);
+    }
 
   CHK_ENTITY_TYPE (ole2frame, OLE2FRAME, unknown, RC, unknown);
 }
