LibreDWG/libredwg

commit 27a4380702667391668d4693ea7bdfdc9cf823c7

free: work on indxf leaks
Keep the first BLOCK_HEADER name static, as all the others.
See GH #151

src/in_dxf.c
@@ -7033,15 +7033,14 @@ dwg_read_dxf (Bit_Chain *restrict dat, Dwg_Data *restrict dwg)
       Dwg_Object *obj;
       Dwg_Object_BLOCK_HEADER *_obj;
       char *dxfname = (char *)"BLOCK_HEADER";
-      char *name = (char *)"*Model_Space";
       NEW_OBJECT (dwg, obj);
       ADD_OBJECT (BLOCK_HEADER);
       // dwg->header.version here still unknown. <r2000: 0x17
       // later fixed up when reading the BLOCK_HEADER.name
       if (dwg->header.version >= R_2007)
-        _obj->name = (BITCODE_T)bit_utf8_to_TU (name);
+        _obj->name = (char*)"*\0M\0o\0d\0e\0l\0_\0S\0p\0a\0c\0e\0";
       else
-        _obj->name = strdup (name);
+        _obj->name = (char*)"*Model_Space";
       obj->tio.object->xdic_missing_flag = 1;
       _obj->xrefref = 1;
       dwg_add_handle (&obj->handle, 0, 0x1F, obj);

commit c9d6e555de0b37a002a713e8bdb9f7deb48ab46f

indxf: fix name "*Model_Space" free
must not be constant. broken with
27a4380702667391668d4693ea7bdfdc9cf823c7

src/in_dxf.c
@@ -7127,9 +7127,9 @@ dwg_read_dxf (Bit_Chain *restrict dat, Dwg_Data *restrict dwg)
       // dwg->header.version here still unknown. <r2000: 0x17
       // later fixed up when reading the BLOCK_HEADER.name
       if (dwg->header.version >= R_2007)
-        _obj->name = (char*)"*\0M\0o\0d\0e\0l\0_\0S\0p\0a\0c\0e\0";
+        _obj->name = strdup ((char*)"*\0M\0o\0d\0e\0l\0_\0S\0p\0a\0c\0e\0");
       else
-        _obj->name = (char*)"*Model_Space";
+        _obj->name = strdup ((char*)"*Model_Space");
       obj->tio.object->xdic_missing_flag = 1;
       _obj->xrefref = 1;
       dwg_add_handle (&obj->handle, 0, 0x1F, obj);