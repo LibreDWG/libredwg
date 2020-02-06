99bf9d06e3d66c9d6ace8c9dc9c081d12276d157
Reini Urban <rurban@cpan.org>
2020-02-06 11:32:48 +0100

    fixed unit-testing/ole2frame
    now with proper TF support

--- test/unit-testing/ole2frame.c
+++ test/unit-testing/ole2frame.c
@@ -8,7 +8,8 @@ api_process (dwg_object *obj)
   BITCODE_BS type, mode;
   BITCODE_BL data_size;
   BITCODE_RC lock_aspect;
-  char *data, *data1;
+  BITCODE_TF data;
+  BITCODE_TF data1;
 
   dwg_ent_ole2frame *ole2frame = dwg_object_to_OLE2FRAME (obj);
 
@@ -17,17 +18,5 @@ api_process (dwg_object *obj)
   CHK_ENTITY_TYPE (ole2frame, OLE2FRAME, lock_aspect, RC, lock_aspect);
 
   CHK_ENTITY_TYPE_W_OLD (ole2frame, OLE2FRAME, data_size, BL, data_size);
-  // CHK_ENTITY_TYPE_W_OLD (ole2frame, OLE2FRAME, data, RC*, data);
-  if (!dwg_dynapi_entity_value (ole2frame, "OLE2FRAME", "data", &data, NULL))
-    fail ("OLE2FRAME.data");
-  data1 = dwg_ent_ole2frame_get_data (ole2frame, &error);
-  // TODO wrong dynapi with TF?
-  if (error || data != data1
-#if 0
-      || memcmp (&data, &data1, data_size)
-#endif
-  )
-    fail ("TODO: dynapi data: %p, old api data: %p", data, data1);
-  else
-    ok ("OLE2FRAME.data: %p == old api data: %p", data, data1);
+  CHK_ENTITY_TYPE_W_OLD (ole2frame, OLE2FRAME, data, TF, data);
 }
