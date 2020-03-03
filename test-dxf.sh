b240c7b2ff285eea6934407aff832f12a30b7775
Reini Urban <rurban@cpan.org>
2020-03-03 07:37:24 +0100

    even more shell scripts in top_srcdir
    to test with shellcheck, and fix quoting and other issues.

--- test-dxf.sh
+++ test-dxf.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 # see also dxf-roundtrip.sh
-arg=${1:-sample_2000}
-dir=$(dirname $arg)
-base=$(basename $arg)
-if [ x$dir = x. ]; then out=${base};
-else out=${base}_${dir}
+arg="${1:-sample_2000}"
+dir="$(dirname "$arg")"
+base="$(basename "$arg")"
+if [ "$dir" = "." ]; then out="${base}";
+else out="${base}_${dir}"
 fi
 echo "$arg => $out.dxf"
 
 make -s -j4 && \
-  echo programs/dwg2dxf -v4 -o $out.dxf test/test-data/$arg.dwg && \
-  programs/dwg2dxf -v4 -o $out.dxf test/test-data/$arg.dwg 2>${out}.log && \
-  programs/filt_dxf.pl $out.dxf > _${out}.dxf; mv _${out}.dxf ${out}.dxf;
+  echo programs/dwg2dxf -v4 -o "$out.dxf" "test/test-data/$arg.dwg" && \
+  programs/dwg2dxf -v4 -o "$out.dxf" "test/test-data/$arg.dwg" 2>"${out}.log" && \
+  programs/filt_dxf.pl "$out.dxf" > "_${out}.dxf"; mv "_${out}.dxf" "${out}.dxf";
 
-egrep -v -B1 '\r' ${out}.dxf | head && unix2dos ${out}.dxf
+grep -E -v -B1 '\r' "${out}.dxf" | head && unix2dos "${out}.dxf"
 
-if [ -f test/test-data/$arg.dxf ]; then
-    diff -bu test/test-data/$arg.dxf $out.dxf | programs/cmp_dxf.pl |less
-elif [ -f test/test-data-$dir/$arg.dxf ]; then
-  diff -bu test/test-data-$dir/$arg.dxf $out.dxf | programs/cmp_dxf.pl |less
+if [ -f "test/test-data/$arg.dxf" ]; then
+    diff -bu "test/test-data/$arg.dxf" "$out.dxf" | programs/cmp_dxf.pl | less
+elif [ -f "test/test-data-$dir/$arg.dxf" ]; then
+  diff -bu "test/test-data-$dir/$arg.dxf" "$out.dxf" | programs/cmp_dxf.pl | less
 fi
--- unknown.sh
+++ unknown.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+make -s -j4 -C src
 make -s -j4 -C examples unknown
 perl -anle'm{/\* } && print $F[1]' examples/alldxf_1.inc | sort -u > unknown.lst
-for s in `cat unknown.lst`
+while IFS= read -r s
 do
-    examples/unknown --class $s > unknown-$s.log
+    examples/unknown --class "$s" > "unknown-$s.log"
+    # shellcheck disable=SC2039
     /bin/echo -n "$s "
-    tail -n1 unknown-$s.log
-done
+    tail -n1 "unknown-$s.log"
+done < unknown.lst
