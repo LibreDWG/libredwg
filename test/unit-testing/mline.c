7e2f99c64687ba97c3b8120835217acc49dfd7d8
Reini Urban <rurban@cpan.org>
2020-03-13 20:26:30 +0100

    unit-testing: extend mline, fix leak

--- test/unit-testing/mline.c
+++ test/unit-testing/mline.c
@@ -20,7 +20,7 @@ api_process (dwg_object *obj)
   CHK_ENTITY_3RD_W_OLD (mline, MLINE, base_point, base_point);
   CHK_ENTITY_3RD_W_OLD (mline, MLINE, extrusion, ext);
   CHK_ENTITY_TYPE_W_OLD (mline, MLINE, flags, BS, flags);
-  CHK_ENTITY_TYPE_W_OLD (mline, MLINE, num_lines, RC, num_lines);
+  CHK_ENTITY_TYPE_W_OLD (mline, MLINE, num_lines, RCd, num_lines);
   CHK_ENTITY_TYPE_W_OLD (mline, MLINE, num_verts, BS, num_verts);
 
   if (!dwg_dynapi_entity_value (mline, "MLINE", "verts", &v1, NULL))
@@ -32,12 +32,35 @@ api_process (dwg_object *obj)
     {
       for (i = 0; i < num_verts; i++)
         {
-          ok ("MLINE.vertex[%d]: (%f, %f, %f)", i, verts[i].vertex.x,
+          dwg_mline_line *lines = dwg_mline_vertex_get_lines (&verts[i], &error);
+          if (error)
+            fail ("MLINE.verts[%d].lines", i);
+          else
+            {
+              for (int j = 0; j < num_lines; j++)
+                {
+                  for (int k = 0; k < lines[j].num_segparms; k++)
+                    {
+                      ok ("MLINE.verts[%d].lines[%d].segparms[%d]: %f", i, j, k,
+                          lines[j].segparms[k]);
+                    }
+                  for (int k = 0; k < lines[j].num_areafillparms; k++)
+                    {
+                      ok ("MLINE.verts[%d].lines[%d].areafillparms[%d]: %f", i, j, k,
+                          lines[j].areafillparms[k]);
+                    }
+                }
+            }
+          free (lines);
+
+          ok ("MLINE.verts[%d]: (%f, %f, %f)", i, verts[i].vertex.x,
               verts[i].vertex.y, verts[i].vertex.z);
           if (memcmp (&v1[i].vertex, &verts[i].vertex,
                       sizeof (verts[i].vertex)))
             fail ("MLINE.verts[%d]", i);
         }
     }
+  free (verts);
+
   CHK_ENTITY_H (mline, MLINE, mlinestyle, mlinestyle);
 }
