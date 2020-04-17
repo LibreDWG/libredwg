9c162a088675f1814bf7fc18053bb8a651e191be
Reini Urban <rurban@cpan.org>
2020-04-17 08:53:29 +0200

    test-dxf.sh: adjust for one-level outoftree builddir

--- test-dxf.sh
+++ test-dxf.sh
@@ -6,17 +6,26 @@ base="$(basename "$arg")"
 if [ "$dir" = "." ]; then out="${base}";
 else out="${base}_${dir}"
 fi
+cmpdxf="programs/cmp_dxf.pl"
+filtdxf="programs/filt_dxf.pl"
+datadir="test/test-data"
+if [ ! -f "$cmpdxf" ]
+then
+    cmpdxf="../programs/cmp_dxf.pl"
+    filtdxf="../programs/filt_dxf.pl"
+    datadir="../test/test-data"
+fi
 echo "$arg => $out.dxf"
 
 make -s -j4 && \
-  echo programs/dwg2dxf -v4 -o "$out.dxf" "test/test-data/$arg.dwg" && \
-  programs/dwg2dxf -v4 -o "$out.dxf" "test/test-data/$arg.dwg" 2>"${out}.log" && \
-  programs/filt_dxf.pl "$out.dxf" > "_${out}.dxf"; mv "_${out}.dxf" "${out}.dxf";
+  echo programs/dwg2dxf -v4 -o "$out.dxf" "$datadir/$arg.dwg" && \
+  programs/dwg2dxf -v4 -o "$out.dxf" "$datadir/$arg.dwg" 2>"${out}.log" && \
+  $filtdxf "$out.dxf" > "_${out}.dxf"; mv "_${out}.dxf" "${out}.dxf";
 
 grep -E -v -B1 '\r' "${out}.dxf" | head && unix2dos "${out}.dxf"
 
-if [ -f "test/test-data/$arg.dxf" ]; then
-    diff -bu "test/test-data/$arg.dxf" "$out.dxf" | programs/cmp_dxf.pl | less
-elif [ -f "test/test-data-$dir/$arg.dxf" ]; then
-  diff -bu "test/test-data-$dir/$arg.dxf" "$out.dxf" | programs/cmp_dxf.pl | less
+if [ -f "$datadir/$arg.dxf" ]; then
+    diff -bu "$datadir/$arg.dxf" "$out.dxf" | $cmpdxf | less
+elif [ -f "$datadir-$dir/$arg.dxf" ]; then
+  diff -bu "$datadir-$dir/$arg.dxf" "$out.dxf" | $cmpdxf | less
 fi
