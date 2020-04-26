634245b95b46b895cf6915b5c0b7d01c30aed2b3
Reini Urban <rurban@cpan.org>
2020-04-26 12:58:59 +0200

    set dat.from_version in converters
    esp. with in_json.
    this enables IF_ENCODE_FROM_EARLIER, e.g. in r2004_file_header.spec

--- programs/dxf2dwg.c
+++ programs/dxf2dwg.c
@@ -341,8 +341,8 @@ main (int argc, char *argv[])
         {
           printf (" as %s\n", version);
           dwg.header.version = dwg_version;
-          if (dwg_version > R_2000)
-            printf ("Warning: encode currently only works for R13-R2000.\n");
+          if (dwg_version == R_2007 || dwg_version < R_13)
+            printf ("Warning: encode currently does not work for pre-r13 and r2007.\n");
           if (dwg.header.from_version == R_INVALID)
             dwg.header.from_version = dwg.header.version;
         }
