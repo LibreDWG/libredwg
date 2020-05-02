08d21be7981e4be1d32e522872e05d24d4cce1c9
Reini Urban <rurban@cpan.org>
2020-05-02 18:10:37 +0200

    fixup layer_index.c unit-test

--- test/unit-testing/layer_index.c
+++ test/unit-testing/layer_index.c
@@ -5,25 +5,19 @@ void
 api_process (dwg_object *obj)
 {
   int error, isnew;
-  BITCODE_BL timestamp1;
-  BITCODE_BL timestamp2;
   BITCODE_BL i, num_entries;
   Dwg_LAYER_entry* entries;
 
   Dwg_Version_Type dwg_version = obj->parent->header.version;
   dwg_obj_layer_index *_obj = dwg_object_to_LAYER_INDEX (obj);
 
-  CHK_ENTITY_TYPE (_obj, LAYER_INDEX, timestamp1, BL, timestamp1);
-  CHK_ENTITY_TYPE (_obj, LAYER_INDEX, timestamp2, BL, timestamp2);
   CHK_ENTITY_TYPE (_obj, LAYER_INDEX, num_entries, BL, num_entries);
   if (!dwg_dynapi_entity_value (_obj, "LAYER_INDEX", "entries", &entries, NULL))
     fail ("LAYER_INDEX.entries");
-  //hdls = _obj->entries;
   for (i = 0; i < num_entries; i++)
     {
-      CHK_SUBCLASS_TYPE (entries[i], LAYER_entry, numlayers, BL);
       CHK_SUBCLASS_UTF8TEXT (entries[i], LAYER_entry, name);
       CHK_SUBCLASS_H (entries[i], LAYER_entry, handle);
-      //ok ("LAYER_INDEX.layer_entries[%d]: " FORMAT_REF, i, ARGS_REF (layer_entries[i]));
+      CHK_SUBCLASS_TYPE (entries[i], LAYER_entry, numlayers, BL);
     }
 }
