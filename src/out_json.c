1e59b42ff8ea328e9da567a847e806e8861e7cad
Reini Urban <rurban@cpan.org>
2020-05-03 14:13:35 +0200

    TABLESTYLE: more embedded structs
    inline them all.
    need to support embedded structs in in_json. for now we just
    had embedded arrays.

--- src/out_json.c
+++ src/out_json.c
@@ -125,6 +125,9 @@ static char *_path_field (const char *path);
 #define RECORD(nam)                                                           \
   KEY (nam);                                                                  \
   HASH
+#define RECORDs(x)                                                            \
+  KEYs (_XSTR (x));                                                           \
+  HASH
 #define ENDRECORD() ENDHASH
 // a named list
 #define SECTION(nam)                                                          \
