2838e5975f669914edce88a2b0a615d91a151f15
Reini Urban <rurban@cpan.org>
2020-05-10 22:31:24 +0200

    spec: add FCFOBJECTCONTEXTDATA
    debugging

---
+++ test/unit-testing/fcfobjectcontextdata.c
@@ -0,0 +1,28 @@
+// TODO DEBUGGING
+#define DWG_TYPE DWG_TYPE_FCFOBJECTCONTEXTDATA
+#include "common.c"
+
+void
+api_process (dwg_object *obj)
+{
+  int error;
+  BITCODE_BS class_version; /*!< r2010+ =3 */
+  BITCODE_B is_default;
+  BITCODE_B in_dwg;
+  BITCODE_H scale; /* DXF 340 */
+
+  BITCODE_3BD location;   /*!< DXF 10-30 */
+  BITCODE_3BD horiz_dir;  /*!< DXF 11-31 */
+
+  Dwg_Version_Type dwg_version = obj->parent->header.version;
+#ifdef DEBUG_CLASSES
+  dwg_obj_fcfobjectcontextdata *_obj = dwg_object_to_FCFOBJECTCONTEXTDATA (obj);
+
+  CHK_ENTITY_TYPE (_obj, FCFOBJECTCONTEXTDATA, class_version, BS, class_version);
+  CHK_ENTITY_TYPE (_obj, FCFOBJECTCONTEXTDATA, is_default, B, is_default);
+  CHK_ENTITY_TYPE (_obj, FCFOBJECTCONTEXTDATA, in_dwg, B, in_dwg);
+  CHK_ENTITY_H (_obj, FCFOBJECTCONTEXTDATA, scale, scale);
+  CHK_ENTITY_3RD (_obj, FCFOBJECTCONTEXTDATA, location_pt, location_pt);
+  CHK_ENTITY_3RD (_obj, FCFOBJECTCONTEXTDATA, horiz_dir, horiz_dir);
+#endif
+}
