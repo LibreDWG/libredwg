4fa42a5e0b06a0cb3bccc92084ac5a17e81b19e6
Reini Urban <rurban@cpan.org>
2020-05-12 07:47:48 +0200

    indxf: rename cur_vstyle var to prev_vstyle

--- src/in_dxf.c
+++ src/in_dxf.c
@@ -7129,7 +7129,7 @@ new_object (char *restrict name, char *restrict dxfname,
               const Dwg_DYNAPI_field *f;
               const Dwg_DYNAPI_field *fields
                   = dwg_dynapi_entity_fields (obj->name);
-              const Dwg_DYNAPI_field *cur_vstyle = NULL;
+              const Dwg_DYNAPI_field *prev_vstyle = NULL;
               if (!pair)
                 break;
               if (!fields)
@@ -7222,13 +7222,13 @@ new_object (char *restrict name, char *restrict dxfname,
                     }
                   else if (obj->fixedtype == DWG_TYPE_VISUALSTYLE &&
                            dat->version >= R_2010 &&
-                           pair->code == 176 && cur_vstyle)
+                           pair->code == 176 && prev_vstyle)
                     {
                       // which 176 of the many? the one after the previous field
                       char fieldname[40];
-                      strcpy (fieldname, cur_vstyle->name);
+                      strcpy (fieldname, prev_vstyle->name);
                       strcat (fieldname, "_int");
-                      f = cur_vstyle + 1;
+                      f = prev_vstyle + 1;
                       if (strEQc (fieldname, "display_brightness_bl_int"))
                         {
                           strcpy (fieldname, "display_brightness_int");
@@ -7249,7 +7249,7 @@ new_object (char *restrict name, char *restrict dxfname,
                           dat->version >= R_2010 &&
                           pair->code != 176)
                         {
-                          cur_vstyle = f;
+                          prev_vstyle = f;
                         }
                       // exceptions, where there's another field 92:
                       if ((pair->code == 92) && is_entity
