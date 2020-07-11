034f898fe2316475e5f9b6f80cf4c06971bd0ed2
Reini Urban <rurban@cpan.org>
2020-07-11 20:42:05 +0200

    WIP convert TU/T strings to Dwg_String*
    with src codepage to avoid double-conversions.
    See GH #245

--- test/unit-testing/dynapi_test.c
+++ test/unit-testing/dynapi_test.c
@@ -19590,14 +19590,14 @@ static int test_POINTCLOUD (const Dwg_Object *obj)
     pointcloud->show_intensity--;
   }
   {
-    BITCODE_TV* source_files;
+    BITCODE_T** source_files;
     BITCODE_BL count = 0;
     if (dwg_dynapi_entity_value (pointcloud, "POINTCLOUD", "num_source_files", &count, NULL)
         && dwg_dynapi_entity_value (pointcloud, "POINTCLOUD", "source_files", &source_files, NULL)
-        && source_files == pointcloud->source_files)
+)
       pass ();
     else
-      fail ("POINTCLOUD.source_files [TV*] * %u num_source_files", count);
+      fail ("POINTCLOUD.source_files [T**] * %u num_source_files", count);
   }
   {
     BITCODE_T ucs_name;
@@ -30009,14 +30009,14 @@ static int test_APPID (const Dwg_Object *obj)
     appid->is_xref_resolved--;
   }
   {
-    BITCODE_TV name;
+    BITCODE_T name;
     if (dwg_dynapi_entity_value (appid, "APPID", "name", &name, NULL)
         && name
            ? strEQ ((char *)name, (char *)appid->name)
            : !appid->name)
       pass ();
     else
-      fail ("APPID.name [TV] '%s' <> '%s'", name, appid->name);
+      fail ("APPID.name [T] '%s' <> '%s'", name, appid->name);
   }
   {
     struct _dwg_object_object* parent;
@@ -39951,14 +39951,14 @@ static int test_BLOCKLOOKUPACTION (const Dwg_Object *obj)
         fail ("BLOCKLOOKUPACTION.evalexpr [Dwg_EvalExpr]");
   }
   {
-    BITCODE_TV* exprs;
+    BITCODE_T** exprs;
     BITCODE_BL count = 0;
     if (dwg_dynapi_entity_value (blocklookupaction, "BLOCKLOOKUPACTION", "num_exprs", &count, NULL)
         && dwg_dynapi_entity_value (blocklookupaction, "BLOCKLOOKUPACTION", "exprs", &exprs, NULL)
-        && exprs == blocklookupaction->exprs)
+)
       pass ();
     else
-      fail ("BLOCKLOOKUPACTION.exprs [TV*] * %u num_exprs", count);
+      fail ("BLOCKLOOKUPACTION.exprs [T**] * %u num_exprs", count);
   }
   {
     Dwg_BLOCKLOOKUPACTION_lut* lut;
@@ -44922,14 +44922,14 @@ static int test_BLOCK_HEADER (const Dwg_Object *obj)
     block_header->loaded_bit--;
   }
   {
-    BITCODE_TV name;
+    BITCODE_T name;
     if (dwg_dynapi_entity_value (block_header, "BLOCK_HEADER", "name", &name, NULL)
         && name
            ? strEQ ((char *)name, (char *)block_header->name)
            : !block_header->name)
       pass ();
     else
-      fail ("BLOCK_HEADER.name [TV] '%s' <> '%s'", name, block_header->name);
+      fail ("BLOCK_HEADER.name [T] '%s' <> '%s'", name, block_header->name);
   }
   {
     BITCODE_RL num_inserts;
@@ -46265,14 +46265,14 @@ static int test_DICTIONARY (const Dwg_Object *obj)
         fail ("DICTIONARY.parent [struct _dwg_object_object*]");
   }
   {
-    BITCODE_T* texts;
+    BITCODE_T** texts;
     BITCODE_BL count = 0;
     if (dwg_dynapi_entity_value (dictionary, "DICTIONARY", "numitems", &count, NULL)
         && dwg_dynapi_entity_value (dictionary, "DICTIONARY", "texts", &texts, NULL)
-        && texts == dictionary->texts)
+)
       pass ();
     else
-      fail ("DICTIONARY.texts [T*] * %u numitems", count);
+      fail ("DICTIONARY.texts [T**] * %u numitems", count);
   }
   if (failed && (is_class_unstable ("DICTIONARY") || is_class_debugging ("DICTIONARY")))
     {
@@ -46415,14 +46415,14 @@ static int test_DICTIONARYWDFLT (const Dwg_Object *obj)
         fail ("DICTIONARYWDFLT.parent [struct _dwg_object_object*]");
   }
   {
-    BITCODE_T* texts;
+    BITCODE_T** texts;
     BITCODE_BL count = 0;
     if (dwg_dynapi_entity_value (dictionarywdflt, "DICTIONARYWDFLT", "numitems", &count, NULL)
         && dwg_dynapi_entity_value (dictionarywdflt, "DICTIONARYWDFLT", "texts", &texts, NULL)
-        && texts == dictionarywdflt->texts)
+)
       pass ();
     else
-      fail ("DICTIONARYWDFLT.texts [T*] * %u numitems", count);
+      fail ("DICTIONARYWDFLT.texts [T**] * %u numitems", count);
   }
   if (failed && (is_class_unstable ("DICTIONARYWDFLT") || is_class_debugging ("DICTIONARYWDFLT")))
     {
@@ -50582,14 +50582,14 @@ static int test_LAYERFILTER (const Dwg_Object *obj)
       return 1;
     }
   {
-    BITCODE_TV* names;
+    BITCODE_T** names;
     BITCODE_BL count = 0;
     if (dwg_dynapi_entity_value (layerfilter, "LAYERFILTER", "num_names", &count, NULL)
         && dwg_dynapi_entity_value (layerfilter, "LAYERFILTER", "names", &names, NULL)
-        && names == layerfilter->names)
+)
       pass ();
     else
-      fail ("LAYERFILTER.names [TV*] * %u num_names", count);
+      fail ("LAYERFILTER.names [T**] * %u num_names", count);
   }
   {
     BITCODE_BL num_names;
@@ -51321,14 +51321,14 @@ static int test_LTYPE (const Dwg_Object *obj)
     ltype->is_xref_resolved--;
   }
   {
-    BITCODE_TV name;
+    BITCODE_T name;
     if (dwg_dynapi_entity_value (ltype, "LTYPE", "name", &name, NULL)
         && name
            ? strEQ ((char *)name, (char *)ltype->name)
            : !ltype->name)
       pass ();
     else
-      fail ("LTYPE.name [TV] '%s' <> '%s'", name, ltype->name);
+      fail ("LTYPE.name [T] '%s' <> '%s'", name, ltype->name);
   }
   {
     BITCODE_RC numdashes;
@@ -58257,14 +58257,14 @@ static int test_STYLE (const Dwg_Object *obj)
     style->last_height--;
   }
   {
-    BITCODE_TV name;
+    BITCODE_T name;
     if (dwg_dynapi_entity_value (style, "STYLE", "name", &name, NULL)
         && name
            ? strEQ ((char *)name, (char *)style->name)
            : !style->name)
       pass ();
     else
-      fail ("STYLE.name [TV] '%s' <> '%s'", name, style->name);
+      fail ("STYLE.name [T] '%s' <> '%s'", name, style->name);
   }
   {
     BITCODE_BD oblique_angle;
@@ -59739,14 +59739,14 @@ static int test_UCS (const Dwg_Object *obj)
     ucs->is_xref_resolved--;
   }
   {
-    BITCODE_TV name;
+    BITCODE_T name;
     if (dwg_dynapi_entity_value (ucs, "UCS", "name", &name, NULL)
         && name
            ? strEQ ((char *)name, (char *)ucs->name)
            : !ucs->name)
       pass ();
     else
-      fail ("UCS.name [TV] '%s' <> '%s'", name, ucs->name);
+      fail ("UCS.name [T] '%s' <> '%s'", name, ucs->name);
   }
   {
     BITCODE_H named_ucs;
@@ -60302,14 +60302,14 @@ static int test_VIEW (const Dwg_Object *obj)
         fail ("VIEW.livesection [H]");
   }
   {
-    BITCODE_TV name;
+    BITCODE_T name;
     if (dwg_dynapi_entity_value (view, "VIEW", "name", &name, NULL)
         && name
            ? strEQ ((char *)name, (char *)view->name)
            : !view->name)
       pass ();
     else
-      fail ("VIEW.name [TV] '%s' <> '%s'", name, view->name);
+      fail ("VIEW.name [T] '%s' <> '%s'", name, view->name);
   }
   {
     BITCODE_H named_ucs;
@@ -62883,14 +62883,14 @@ static int test_VPORT (const Dwg_Object *obj)
         fail ("VPORT.lower_left [2RD]");
   }
   {
-    BITCODE_TV name;
+    BITCODE_T name;
     if (dwg_dynapi_entity_value (vport, "VPORT", "name", &name, NULL)
         && name
            ? strEQ ((char *)name, (char *)vport->name)
            : !vport->name)
       pass ();
     else
-      fail ("VPORT.name [TV] '%s' <> '%s'", name, vport->name);
+      fail ("VPORT.name [T] '%s' <> '%s'", name, vport->name);
   }
   {
     BITCODE_H named_ucs;
@@ -63303,14 +63303,14 @@ static int test_VX_TABLE_RECORD (const Dwg_Object *obj)
     vx_table_record->is_xref_resolved--;
   }
   {
-    BITCODE_TV name;
+    BITCODE_T name;
     if (dwg_dynapi_entity_value (vx_table_record, "VX_TABLE_RECORD", "name", &name, NULL)
         && name
            ? strEQ ((char *)name, (char *)vx_table_record->name)
            : !vx_table_record->name)
       pass ();
     else
-      fail ("VX_TABLE_RECORD.name [TV] '%s' <> '%s'", name, vx_table_record->name);
+      fail ("VX_TABLE_RECORD.name [T] '%s' <> '%s'", name, vx_table_record->name);
   }
   {
     struct _dwg_object_object* parent;
@@ -67918,14 +67918,6 @@ test_sizes (void)
     {
       fprintf (stderr, "sizeof(struct _dwg_BLOCKPARAMETER_connection): %d != "
                "dwg_dynapi_fields_size (\"BLOCKPARAMETER_connection\"): %d\n", size1, size2);
-      error++;
-    }
-  size1 = sizeof (struct _dwg_BLOCKPARAMVALUESET);
-  size2 = dwg_dynapi_fields_size ("BLOCKPARAMVALUESET");
-  if (size1 != size2)
-    {
-      fprintf (stderr, "sizeof(struct _dwg_BLOCKPARAMVALUESET): %d != "
-               "dwg_dynapi_fields_size (\"BLOCKPARAMVALUESET\"): %d\n", size1, size2);
       error++;
     }
   size1 = sizeof (struct _dwg_BLOCKSTRETCHACTION_codes);
