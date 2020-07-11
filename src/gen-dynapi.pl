b0e4b722a2016146160ffb8f3524cf03a6e881b5
Reini Urban <rurban@cpan.org>
2020-07-11 20:42:05 +0200

    WIP convert TU/T strings to Dwg_String*
    with src codepage to avoid double-conversions.
    See GH #245

--- src/gen-dynapi.pl
+++ src/gen-dynapi.pl
@@ -2869,7 +2869,7 @@ dwg_dynapi_entity_utf8text (void *restrict _obj, const char *restrict name,
       if (is_tu && strNE (f->type, "TF")) /* not TF */
         {
           BITCODE_TU wstr = *(BITCODE_TU*)((char*)_obj + f->offset);
-          char *utf8 = bit_convert_TU (wstr);
+          char *utf8 = bit_TU_to_utf8 (wstr);
           if (wstr && !utf8) // some conversion error, invalid wchar (nyi)
             return false;
           *out = utf8;
@@ -2941,7 +2941,7 @@ dwg_dynapi_header_utf8text (const Dwg_Data *restrict dwg,
         if (is_tu && !is_fixed) /* not TF */
           {
             BITCODE_TU wstr = *(BITCODE_TU*)((char*)_obj + f->offset);
-            char *utf8 = bit_convert_TU (wstr);
+            char *utf8 = bit_TU_to_utf8 (wstr);
             if (wstr && !utf8) // some conversion error, invalid wchar (nyi)
               return false;
             *out = utf8;
@@ -3078,7 +3078,7 @@ dwg_dynapi_common_utf8text(void *restrict _obj, const char *restrict fieldname,
         if (is_tu && strNE (f->type, "TF")) /* not TF */
           {
             BITCODE_TU wstr = *(BITCODE_TU*)((char*)_obj + f->offset);
-            char *utf8 = bit_convert_TU (wstr);
+            char *utf8 = bit_TU_to_utf8 (wstr);
             if (wstr && !utf8) // some conversion error, invalid wchar (nyi)
               return false;
             *out = utf8;
@@ -3535,7 +3535,7 @@ dwg_dynapi_handle_name (const Dwg_Data *restrict dwg,
       {
         BITCODE_TU wstr = *(BITCODE_TU *)((char *)_obj + f->offset);
         *alloced = 1;
-        return bit_convert_TU (wstr);
+        return bit_TU_to_utf8 (wstr);
       }
     else
       {
