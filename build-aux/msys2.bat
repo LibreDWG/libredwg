522e46d70d1ec32badf001b52524be189b71b1b3
Reini Urban <rurban@cpan.org>
2020-10-07 10:38:41 +0200

    msys2: fix broken msys2 mirror
    update to latest image VS 2019, which does not need a complete update.

--- build-aux/msys2.bat
+++ build-aux/msys2.bat
@@ -17,10 +17,13 @@ echo Installation MSYS2 build preconditions...
 echo Extending path to MSYS...
 SET "PATH=C:\%MSYS2_DIR%\%MSYSTEM%\bin;C:\%MSYS2_DIR%\usr\bin;%PATH%"
 
+echo Skipping pacman updates
+goto depends
 echo Temporary keyring mess (https://www.msys2.org/news/)
-bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
-bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
-bash -lc "pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz{.sig,}"
+bash -lc "wget http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz{,.sig}"
+rem bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
+rem bash -lc "curl -O http://repo.msys2.org/msys/x86_64/msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
+bash -lc "pacman-key --verify msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz.sig"
 bash -lc "pacman -U --noconfirm msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
 rem bash -lc "pacman -U --config <(echo) msys2-keyring-r21.b39fb11-1-any.pkg.tar.xz"
 
@@ -38,6 +41,7 @@ rem echo Removing outdated conflicts...
 rem mingw-w64-x86_64-pdcurses and mingw-w64-x86_64-ncurses are in conflict...
 rem bash -lc "pacman -R --noconfirm --nodeps mingw-w64-%MSYS2_ARCH%-python3 mingw-w64-%MSYS2_ARCH%-readline mingw-w64-%MSYS2_ARCH%-sqlite3 mingw-w64-%MSYS2_ARCH%-ncurses mingw-w64-%MSYS2_ARCH%-termcap"
 
+:depends
 echo Installing dependencies...
 rem see https://github.com/Alexpux/MSYS2-packages
 bash -lc "pacman -S --needed --noconfirm mingw-w64-%MSYS2_ARCH%-toolchain mingw-w64-%MSYS2_ARCH%-libtool mingw-w64-%MSYS2_ARCH%-libiconv mingw-w64-%MSYS2_ARCH%-libxml2 mingw-w64-%MSYS2_ARCH%-swig mingw-w64-%MSYS2_ARCH%-python2 texinfo mingw-w64-%MSYS2_ARCH%-perl mingw-w64-%MSYS2_ARCH%-pcre2"
