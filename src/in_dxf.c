1f4dc787b337b27298db2580a5df27230f1c14a2
Reini Urban <rurban@cpan.org>
2021-02-24 16:18:01 +0100

    indxf: protect dwg_read_dxf the same as dxf_read_file
    check if input size is too small, and if it's not a DWG.
    llvmfuzz used the latter, which was not repro with dxf2dwg.

--- src/in_dxf.c
+++ src/in_dxf.c
@@ -12674,6 +12674,7 @@ dwg_read_dxf (Bit_Chain *restrict dat, Dwg_Data *restrict dwg)
   Dxf_Pair *pair;
   int error = 0;
 
+  loglevel = dwg->opts & DWG_OPTS_LOGLEVEL;
   if (!dat->chain && dat->fh)
     {
       error = dat_read_stream (dat, dat->fh);
@@ -12687,7 +12688,21 @@ dwg_read_dxf (Bit_Chain *restrict dat, Dwg_Data *restrict dwg)
           dat->byte = 22;
         }
     }
-  loglevel = dwg->opts & DWG_OPTS_LOGLEVEL;
+  if (dat->size < 256)
+    {
+      LOG_ERROR ("DXF input too small, %lu byte.\n", dat->size);
+      return DWG_ERR_IOERROR;
+    }
+  /* Fail early on DWG */
+  if (!memcmp (dat->chain, "AC10", 4) ||
+      !memcmp (dat->chain, "AC1.", 4) ||
+      !memcmp (dat->chain, "AC2.10", 4) ||
+      !memcmp (dat->chain, "MC0.0", 4))
+    {
+      LOG_ERROR ("This is a DWG, not a DXF\n");
+      return DWG_ERR_INVALIDDWG;
+    }
+
   num_dxf_objs = 0;
   size_dxf_objs = 1000;
   dxf_objs = (Dxf_Objs *)malloc (1000 * sizeof (Dxf_Objs));
