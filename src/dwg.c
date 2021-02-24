d84fd5e11399d5d68cf037a057d5672554184372
Reini Urban <rurban@cpan.org>
2021-02-24 09:21:52 +0100

    WIP r14 dxf regression
    break cycle in BLOCK next_entity chain.
    but really the <r2004 entity iterator looks totally broken to me.
    it goes into BLOCK entities, which don't belong to the owner, e.g. model space.

--- src/dwg.c
+++ src/dwg.c
@@ -1224,6 +1224,7 @@ dwg_next_entity (const Dwg_Object *restrict obj)
       && obj->parent->header.version > R_12)
     {
       if (!obj->tio.entity) // decoding error
+        //return NULL; FIXME!
         goto next_obj;
       next = obj->tio.entity->next_entity;
       if (next && next->absolute_ref)
@@ -1235,7 +1236,8 @@ dwg_next_entity (const Dwg_Object *restrict obj)
                      : next_obj;
         }
       else
-        goto next_obj;
+        goto next_obj; // FIXME!
+        //return NULL;
     }
   else
     {
