5d2c75fd5cc7d7cc2e19e158237737bdaa2a9697
Reini Urban <rurban@cpan.org>
2021-03-01 21:15:13 +0100

    decode_r2007: fail earlier with illegal system page data
    to avoid integer overflow in rounded calcs.
    Better fix for oss-fuzz issue 31574

--- src/decode_r2007.c
+++ src/decode_r2007.c
@@ -628,47 +628,37 @@ read_system_page (Bit_Chain *dat, int64_t size_comp, int64_t size_uncomp,
   BITCODE_RC *pedata; // Pre RS encoded data
   BITCODE_RC *data;   // The data RS unencoded and uncompressed
 
-  if (repeat_count < 0 || repeat_count > DBG_MAX_COUNT)
+  if (repeat_count < 0 ||
+      repeat_count > DBG_MAX_COUNT ||
+      (uint64_t)size_comp >= dat->size ||
+      (uint64_t)size_uncomp >= dat->size)
     {
-      LOG_ERROR ("Invalid r2007 system page repeat_count. size_comp: %" PRId64
-                 ", repeat_count: %" PRId64, size_comp, repeat_count);
+      LOG_ERROR ("Invalid r2007 system page: "
+                 "size_comp: %" PRId64 ", size_uncomp: %" PRId64
+                 ", repeat_count: %" PRId64, size_comp, size_uncomp, repeat_count);
       return NULL;
     }
   // Round to a multiple of 8
   pesize = ((size_comp + 7) & ~7) * repeat_count;
   // Divide pre encoded size by RS k-value (239)
   block_count = (pesize + 238) / 239;
-  if (!block_count)
+  if (block_count <= 0 || block_count > DBG_MAX_COUNT)
     {
-      LOG_ERROR ("Empty r2007 system page block_count. size_comp: %" PRId64
-                 ", repeat_count: %" PRId64,
-                 size_comp, repeat_count);
+      LOG_ERROR ("Invalid r2007 system page: size_comp: %" PRId64
+                 ", size_uncomp: %" PRId64, size_comp, size_uncomp);
       return NULL;
     }
-
   // Multiply with codeword size (255) and round to a multiple of 8
   page_size = (block_count * 255 + 7) & ~7;
-
-  if (!((uint64_t)size_comp < dat->size &&
-        (uint64_t)size_uncomp < dat->size &&
-        (uint64_t)repeat_count < DBG_MAX_COUNT &&
-        (uint64_t)page_size < DBG_MAX_COUNT))
+  if ((uint64_t)page_size >= DBG_MAX_COUNT || (unsigned long)page_size > dat->size - dat->byte)
     {
-      LOG_ERROR ("Invalid r2007 system page size_comp: %" PRId64
-                 ", size_uncomp: %" PRId64, size_comp, size_uncomp);
+      LOG_ERROR ("Invalid r2007 system page: page_size: %" PRId64, page_size);
       return NULL;
     }
   assert ((uint64_t)size_comp < dat->size);
   assert ((uint64_t)size_uncomp < dat->size);
   assert ((uint64_t)repeat_count < DBG_MAX_COUNT);
   assert ((uint64_t)page_size < DBG_MAX_COUNT);
-
-  if ((unsigned long)page_size > dat->size - dat->byte) // bytes left to read
-    {
-      LOG_ERROR ("Invalid page_size %ld > %lu bytes left", (long)page_size,
-                 dat->size - dat->byte);
-      return NULL;
-    }
   data = (BITCODE_RC *)calloc (size_uncomp + page_size, 1);
   LOG_HANDLE ("Alloc system page of size %" PRId64 "\n", size_uncomp + page_size)
   if (!data)
