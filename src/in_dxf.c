1f291651e70ebe7a2b808e1f6ae4f15770aeeea8
Reini Urban <rurban@cpan.org>
2021-03-09 19:31:17 +0100

    indxf: better dxf_skip_ws overflow protection
    May fix oss-fuzz issue 31789, but not repro

--- src/in_dxf.c
+++ src/in_dxf.c
@@ -229,6 +229,8 @@ dxf_skip_ws (Bit_Chain *dat)
   const int is_binary = dat->opts & DWG_OPTS_DXFB;
   if (is_binary)
     return;
+  if (dat->byte >= dat->size)
+    return;
   // clang-format off
   for (; (!dat->chain[dat->byte] ||
           dat->chain[dat->byte] == ' ' ||
