29d39a6ac581aa5a1c9d29056f77410e7be14438
Reini Urban <rurban@cpan.org>
2021-10-04 16:59:06 +0200

    update for latest shellcheck
    which came with new warnings

--- unknown.sh
+++ unknown.sh
@@ -5,7 +5,7 @@ perl -anle'm{/\* } && print $F[1]' examples/alldxf_1.inc | sort -u > unknown.lst
 while IFS= read -r s
 do
     examples/unknown --class "$s" > "unknown-$s.log"
-    # shellcheck disable=SC2039
+    # shellcheck disable=SC2039,SC3037
     /bin/echo -n "$s "
     tail -n1 "unknown-$s.log"
 done < unknown.lst
