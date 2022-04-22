LibreDWG/libredwg

commit 8f116dd9b79b587f163cb0bfafc33b49a8d503db

Fix pre R_2_0 shape entity

src/dwg.spec
@@ -1860,7 +1860,12 @@ DWG_ENTITY_END
 DWG_ENTITY (SHAPE)
 
   SUBCLASS (AcDbShape)
-  PRE (R_13) {
+  PRE (R_2_0) {
+    FIELD_2RD (ins_pt, 0);
+    FIELD_RD (height_pre2, 0);
+    FIELD_RD (oblique_angle, 0);
+    FIELD_RS (style_id, 0);
+  } VERSIONS (R_2_0, R_13) {
     FIELD_HANDLE (style, 5, 0);
     FIELD_2RD (ins_pt, 10);
     FIELD_RS (style_id, 0); // dxf: 2
@@ -1869,7 +1874,7 @@ DWG_ENTITY (SHAPE)
     if (R11OPTS (2))
       FIELD_RD (ins_pt.z, 38);
   }
-  LATER_VERSIONS {
+  SINCE (R_13) {
     FIELD_3BD (ins_pt, 10);
     FIELD_BD (scale, 40);  // documented as size
     FIELD_BD0 (rotation, 50);

include/dwg.h
@@ -1809,6 +1809,7 @@ typedef struct _dwg_entity_SHAPE
   BITCODE_BS style_id;
   BITCODE_BE extrusion;
   BITCODE_H style;
+  BITCODE_RD height_pre2; // pre R_2_0
 } Dwg_Entity_SHAPE;
 
 /**

commit 9090db8f7a702c4f1437e9fe3b688b24e38f3209

spec: fix shape spec for r13
broken with 8f116dd9b79b587f163cb0bfafc33b49a8d503db

src/dwg.spec
@@ -1865,7 +1865,7 @@ DWG_ENTITY (SHAPE)
     FIELD_RD (height_pre2, 0);
     FIELD_RD (oblique_angle, 0);
     FIELD_RS (style_id, 0);
-  } VERSIONS (R_2_0, R_13) {
+  } VERSIONS (R_2_0, R_11) {
     FIELD_HANDLE (style, 5, 0);
     FIELD_2RD (ins_pt, 10);
     FIELD_RS (style_id, 0); // dxf: 2
@@ -1874,7 +1874,7 @@ DWG_ENTITY (SHAPE)
     if (R11OPTS (2))
       FIELD_RD (ins_pt.z, 38);
   }
-  SINCE (R_13) {
+  SINCE (R_13b1) {
     FIELD_3BD (ins_pt, 10);
     FIELD_BD (scale, 40);  // documented as size
     FIELD_BD0 (rotation, 50);