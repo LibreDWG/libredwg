c89a89b13074a9cd0823c37aab1a415e9bdbae31
Reini Urban <rurban@cpan.org>
2022-05-01 15:39:14 +0200

    preR13: fix unit-testing/decode_test undefined refs

--- src/decode_r11.h
+++ src/decode_r11.h
@@ -15,7 +15,15 @@
  * written by Reini Urban
  */
 
-int decode_preR13 (Bit_Chain *restrict dat, Dwg_Data *restrict dwg);
-int decode_entity_preR13 (Bit_Chain *restrict dat, Dwg_Object *restrict obj,
-                          Dwg_Object_Entity *ent);
+#ifndef DECODE_R11_H
+#define DECODE_R11_H
 
+// exported only for unit-testing/decode_test
+// not part of the public API
+EXPORT int decode_preR13 (Bit_Chain *restrict dat, Dwg_Data *restrict dwg);
+EXPORT int decode_entity_preR13 (Bit_Chain *restrict dat,
+                                 Dwg_Object *restrict obj,
+                                 Dwg_Object_Entity *ent);
+EXPORT Dwg_Object_Ref *dwg_decode_preR13_handleref (Bit_Chain *restrict dat,
+                                                    int size);
+#endif
