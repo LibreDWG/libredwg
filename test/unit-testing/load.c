396a38cfe3c9561074754107b7962fa756ed9ec7
Reini Urban <rurban@cpan.org>
2022-12-11 18:37:38 +0100

    unit-tests: fix load field

--- test/unit-testing/load.c
+++ test/unit-testing/load.c
@@ -7,10 +7,10 @@ api_process (dwg_object *obj)
 {
 #ifdef USE_WRITE
   int error;
-  char *name;
+  char *file_name;
   int isnew;
   dwg_ent_load *load = dwg_object_to_LOAD (obj);
 
-  CHK_ENTITY_UTF8TEXT (load, LOAD, name);
+  CHK_ENTITY_UTF8TEXT (load, LOAD, file_name);
 #endif
 }
