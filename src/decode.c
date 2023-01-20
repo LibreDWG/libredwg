764d45568ae31acf5a36b3960ecc674b4ee0f152
Reini Urban <rurban@cpan.org>
2023-01-20 08:28:27 +0100

    decode: minor 0x8000 harmonization

--- src/decode.c
+++ src/decode.c
@@ -5850,7 +5850,7 @@ decode_preR13_DIMENSION (Bit_Chain *restrict dat, Dwg_Object *restrict obj)
           FIELD_RD0 (oblique_angle, 52); // ext_line_rotation
         if (R11OPTS (0x400))
           FIELD_RD0 (text_rotation, 53);
-        if (R11OPTS (32768))
+        if (R11OPTS (0x8000))
           FIELD_HANDLE (dimstyle, 2, 0);
       }
       break;
@@ -5877,7 +5877,7 @@ decode_preR13_DIMENSION (Bit_Chain *restrict dat, Dwg_Object *restrict obj)
           FIELD_RD (oblique_angle, 50);
         if (R11OPTS (0x400))
           FIELD_RD0 (text_rotation, 53);
-        if (R11OPTS (32768))
+        if (R11OPTS (0x8000))
           FIELD_HANDLE (dimstyle, 2, 0);
       }
       break;
@@ -5910,7 +5910,7 @@ decode_preR13_DIMENSION (Bit_Chain *restrict dat, Dwg_Object *restrict obj)
           FIELD_2RD (xline2end_pt, 16);
         if (R11OPTS (0x400))
           FIELD_RD0 (text_rotation, 53);
-        if (R11OPTS (32768))
+        if (R11OPTS (0x8000))
           FIELD_HANDLE (dimstyle, 2, 0);
       }
       break;
@@ -5930,7 +5930,7 @@ decode_preR13_DIMENSION (Bit_Chain *restrict dat, Dwg_Object *restrict obj)
           FIELD_RD (leader_len, 40);
         if (R11OPTS (0x400))
           FIELD_RD0 (text_rotation, 53);
-        if (R11OPTS (32768))
+        if (R11OPTS (0x8000))
           FIELD_HANDLE (dimstyle, 2, 0);
       }
       break;
@@ -5950,7 +5950,7 @@ decode_preR13_DIMENSION (Bit_Chain *restrict dat, Dwg_Object *restrict obj)
           FIELD_RD (leader_len, 40);
         if (R11OPTS (0x400))
           FIELD_RD0 (text_rotation, 53);
-        if (R11OPTS (32768))
+        if (R11OPTS (0x8000))
           FIELD_HANDLE (dimstyle, 2, 0);
       }
       break;
@@ -5985,7 +5985,7 @@ decode_preR13_DIMENSION (Bit_Chain *restrict dat, Dwg_Object *restrict obj)
             FIELD_RD0 (horiz_direction, 51);
             FIELD_RD0 (text_rotation, 53); //??
           }
-        if (R11OPTS (32768))
+        if (R11OPTS (0x8000))
           FIELD_HANDLE (dimstyle, 2, 0);
       }
       break;
@@ -6010,7 +6010,7 @@ decode_preR13_DIMENSION (Bit_Chain *restrict dat, Dwg_Object *restrict obj)
           }
         if (R11OPTS (0x400))
           FIELD_RD0 (text_rotation, 53);
-        if (R11OPTS (32768))
+        if (R11OPTS (0x8000))
           FIELD_HANDLE (dimstyle, 5, 0);
       }
       break;
