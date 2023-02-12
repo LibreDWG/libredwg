80f3c7884477e84d70f31a61fa8059992040ae9a
Reini Urban <rurban@cpan.org>
2023-02-12 15:15:02 +0100

    free: fix block_control.entries leak/double-free preR13
    We know the number of tables, but add_ may add some beforehand.

--- src/decode_r11.c
+++ src/decode_r11.c
@@ -311,9 +311,16 @@ decode_preR13_section (Dwg_Section_Type_r11 id, Bit_Chain *restrict dat,
     if (ctrl)                                                                 \
       {                                                                       \
         _ctrl = ctrl->tio.object->tio.token##_CONTROL;                        \
-        _ctrl->entries = calloc (tbl->number, sizeof (BITCODE_H));            \
-        _ctrl->num_entries = tbl->number;                                     \
-        LOG_TRACE (#token "_CONTROL.num_entries = %u\n", tbl->number);        \
+        if (_ctrl->num_entries != tbl->number)                                \
+          {                                                                   \
+            if (_ctrl->entries)                                               \
+              _ctrl->entries = realloc (_ctrl->entries,                       \
+                                        tbl->number * sizeof (BITCODE_H));    \
+            else                                                              \
+              _ctrl->entries = calloc (tbl->number, sizeof (BITCODE_H));      \
+            _ctrl->num_entries = tbl->number;                                 \
+            LOG_TRACE (#token "_CONTROL.num_entries = %u\n", tbl->number);    \
+          }                                                                   \
       }
 
 #  define NEW_OBJECT                                                          \
