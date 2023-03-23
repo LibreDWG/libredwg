590af946568cc1b59af28a6c5fe26f6f25e646f0
Reini Urban <rurban@cpan.org>
2023-03-23 07:54:50 +0100

    bit_utf8_to_TU: implement cquoted option
    yet unused

--- src/bits.c
+++ src/bits.c
@@ -2716,8 +2716,8 @@ bit_TU_to_utf8_len (const BITCODE_TU restrict wstr, const int len)
 }
 
 /** converts UTF-8 (dxf,json) to ASCII TV.
-    Unquotes \" to ", \\ to \,
-    undo json_cquote(), \\uxxxx or other unicode => \\U+XXXX.
+    optionally unquotes \" to ", \\ to \, undo json_cquote(),
+    \\uxxxx or other unicode => \\U+XXXX.
     Returns NULL if not enough room in dest.
 */
 char *
@@ -2853,19 +2853,34 @@ bit_utf8_to_TU (char *restrict str, const unsigned cquoted)
                 (unsigned long)len);
       len = UINT16_MAX - 1;
     }
-  wstr = (BITCODE_TU)malloc (2 * (len + 1));
+  wstr = (BITCODE_TU)calloc (2, len + 1);
   if (!wstr)
     {
       loglevel = 1;
       LOG_ERROR ("Out of memory")
       return NULL;
     }
-  while (len && (c = *str++))
+  while (len > 0 && (c = *str++))
     {
       len--;
       if (c < 128)
         {
-          wstr[i++] = c;
+          if (cquoted && c == '\\' && len > 1 &&
+              // skip \" to " and \\ to \.
+              (*str == '"' || *str == '\\' || *str == 'r'
+               || *str == 'n'))
+            {
+              c = *str++;
+              if (c == 'r')
+                wstr[i++] = '\r';
+              else if (c == 'n')
+                wstr[i++] = '\n';
+              // else skip \ and " from \\ and \"
+              else
+                wstr[i++] = c;
+            }
+          else
+            wstr[i++] = c;
         }
       else if ((c & 0xe0) == 0xc0)
         {
@@ -2899,7 +2914,6 @@ bit_utf8_to_TU (char *restrict str, const unsigned cquoted)
         }
       /* everything above 0xf0 exceeds ucs-2, 4-6 byte seqs */
     }
-  wstr[i] = '\0';
   return wstr;
 }
 
