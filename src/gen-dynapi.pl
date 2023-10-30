20b119e64393c59d0e5ff6f4945f58868a53b1c1
Reini Urban <rurban@cpan.org>
2023-10-30 12:59:55 +0100

    dynapi: fix r11 MENU size
    Fixes ossfuzz 50194 regression

--- src/gen-dynapi.pl
+++ src/gen-dynapi.pl
@@ -3251,7 +3251,7 @@ dwg_dynapi_header_set_value (Dwg_Data *restrict dwg,
         const Dwg_Header_Variables *const _obj = &dwg->header_vars;
         // but there are several fixed-length malloced strings preR13
         static const Dwg_DYNAPI_field r11_fixed_strings[] = {
-          { "MENU", "TFv", 15, OFF (struct _dwg_header_variables, MENU), 1,1,1, 1 },
+          { "MENU", "TFv", 16, OFF (struct _dwg_header_variables, MENU), 1,1,1, 1 },
           { "DIMBLK_T", "TFv", 33, OFF (struct _dwg_header_variables, DIMBLK_T), 1,1,1, 1 },
           { "DIMBLK1_T", "TFv", 33, OFF (struct _dwg_header_variables, DIMBLK1_T), 1,1,1, 1 },
           { "DIMBLK2_T", "TFv", 33, OFF (struct _dwg_header_variables, DIMBLK2_T), 1,1,1, 1 },
