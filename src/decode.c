0cce8593fbbc2fddc63ca2e1314fbff87cbcbecf
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-03-24 16:47:29 +0100

    Log decrypted R2004 header

--- src/decode.c
+++ src/decode.c
@@ -3238,6 +3238,10 @@ decode_R2004_header (Bit_Chain *restrict file_dat, Dwg_Data *restrict dwg)
                 0x80, size + 0x80, size);
     LOG_TF_HEX (HANDLE, &file_dat->chain[0x80], (int)size);
     decrypt_R2004_header (decrypted_data, &file_dat->chain[0x80], size);
+    LOG_HANDLE ("decrypted R2004_Header (@%u.0-%" PRIuSIZE ".0, %" PRIuSIZE
+                "):\n",
+                0x80, size + 0x80, size);
+    LOG_TF_HEX (HANDLE, &decrypted_data[0], size);
 
     dat = &decrypted_header_dat;
     dat->bit = 0;
