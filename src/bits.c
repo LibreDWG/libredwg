LibreDWG/libredwg

commit d868ddf1ba636db265cabc3ce6cf8f4999d03844

encode: write no TV-ZERO with r14
only since r2000

src/bits.c
@@ -2190,8 +2190,8 @@ bit_write_TV (Bit_Chain *restrict dat, BITCODE_TV restrict chain)
     bit_write_RS (dat, (BITCODE_RS)length);
   else
     {
-      if (dat->version > R_13c3 && length)
-        length++;
+      if (dat->version > R_14 && length)
+        length++; // TV-ZERO
       bit_write_BS (dat, (BITCODE_BS)length);
     }
   for (i = 0; i < length; i++)

commit 0ea8e93554ad0dd953928454ae95cf69b26fe689

fix json-check example_r14 roundtrip regression
Revert two changes from d868ddf1b that broke r14 DWG output.

* src/bits.c (bit_write_TV): restore TV-ZERO null terminator for r14
  by using >= R_14 instead of > R_14.  r14 TV fields need the trailing
  \0 byte, otherwise ODA misidentifies class names on re-read.
* src/encode.c (dwg_encode): do not unconditionally overwrite
  maint_rel_version from the dwg_versions table.  When importing from
  JSON the value from the input file (e.g. 0xa for example_r14) must
  be preserved.

src/bits.c
@@ -2201,7 +2201,7 @@ bit_write_TV (Bit_Chain *restrict dat, BITCODE_TV restrict chain)
     bit_write_RS (dat, (BITCODE_RS)length);
   else
     {
-      if (dat->version > R_14 && length)
+      if (dat->version >= R_14 && length)
         length++; // TV-ZERO
       bit_write_BS (dat, (BITCODE_BS)length);
     }

src/encode.c
@@ -2931,7 +2931,7 @@ dwg_encode (Dwg_Data *restrict dwg, Bit_Chain *restrict dat)
           // pre-R13b1 must stay 0 (set by dxf_fixup_header or decode)
           _obj->dwg_version = _verp->dwg_version;
       }
-    if (_verp)
+    if (_verp && !_obj->maint_rel_version)
       _obj->maint_rel_version = _verp->maint_rel_version;
     if (!_obj->app_dwg_version)
       _obj->app_dwg_version = _obj->dwg_version;