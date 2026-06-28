b7439cb5ff03777360116acaa7710a6239a859cb
Reini Urban <reini.urban@gmail.com>
2026-06-28 11:03:07 +0000

    escape: Improved htmlwescape/htmlescape
    grow only only on demand. Can only fail on OOM now.
    See PR #1302

--- programs/escape.c
+++ programs/escape.c
@@ -23,10 +23,23 @@
 #include "common.h"
 #include "escape.h"
 
-#define SAFE_APPEND(d, end, s)                    \
-      if (d + (sizeof(s) - 1) >= end) goto done;  \
-      strcat (d, s);                              \
-      d += (sizeof(s) - 1)
+#define APPEND_GROW(d, dest, end, len, s)                                \
+      if ((d) + (sizeof(s) - 1) >= (end))                                \
+        {                                                                \
+          const int _off = (d) - (dest);                                 \
+          char *_new                                                     \
+              = (char *)realloc ((dest), (len) + (sizeof(s) - 1) + 10);  \
+          if (!_new)                                                     \
+            return NULL;                                                 \
+          (dest) = _new;                                                 \
+          (len) += (sizeof(s) - 1) + 10;                                 \
+          (d) = (dest) + _off;                                           \
+          *(d) = 0;                                                      \
+          (end) = (dest) + (len);                                        \
+        }                                                                \
+      memcpy ((d), (s), sizeof(s) - 1);                                  \
+      (d) += (sizeof(s) - 1);                                            \
+      *(d) = 0
 
 char *ATTRIBUTE_MALLOC
 htmlescape (const char *restrict src, const Dwg_Codepage cp)
@@ -45,49 +58,47 @@ htmlescape (const char *restrict src, const Dwg_Codepage cp)
   end = dest + len;
   while (*s)
     {
-      if (end - d <= 8)
-        {
-          const int off = d - dest;
-          char *newdest = (char *)realloc (dest, len + 10);
-          if (!newdest)
-            return NULL;
-          dest = newdest;
-          len += 10;
-          d = dest + off;
-          *d = 0;
-          end = dest + len;
-        }
-
       switch (*s)
         {
         case '"':
-          SAFE_APPEND (d, end, "&quot;");
+          APPEND_GROW (d, dest, end, len, "&quot;");
           break;
         case '\'':
-          SAFE_APPEND (d, end, "&#39;");
+          APPEND_GROW (d, dest, end, len, "&#39;");
           break;
         case '`':
-          SAFE_APPEND (d, end, "&#96;");
+          APPEND_GROW (d, dest, end, len, "&#96;");
           break;
         case '&':
-          SAFE_APPEND (d, end, "&amp;");
+          APPEND_GROW (d, dest, end, len, "&amp;");
           break;
         case '<':
-          SAFE_APPEND (d, end, "&lt;");
+          APPEND_GROW (d, dest, end, len, "&lt;");
           break;
         case '>':
-          SAFE_APPEND (d, end, "&gt;");
+          APPEND_GROW (d, dest, end, len, "&gt;");
           break;
         case '{':
-          SAFE_APPEND (d, end, "&#123;");
+          APPEND_GROW (d, dest, end, len, "&#123;");
           break;
         case '}':
-          SAFE_APPEND (d, end, "&#125;");
+          APPEND_GROW (d, dest, end, len, "&#125;");
           break;
         default:
           {
             uint16_t cc = *s;
             wchar_t wc;
+            if (end - d <= 16)
+              {
+                const int _off = d - dest;
+                char *_new = (char *)realloc (dest, len + 16);
+                if (!_new)
+                  return NULL;
+                dest = _new;
+                len += 16;
+                d = dest + _off;
+                end = dest + len;
+              }
             if (dwg_codepage_is_twobyte (cp, *s))
               cc = cc << 8 | *++s;
             wc = dwg_codepage_uwc (cp, cc);
@@ -108,7 +119,6 @@ htmlescape (const char *restrict src, const Dwg_Codepage cp)
         }
       s++;
     }
-done:
   *d = 0;
   return dest;
 }
@@ -133,45 +143,44 @@ htmlwescape (BITCODE_TU wstr)
 
   while (*wstr)
     {
-      const int off = d - dest;
-      if (off >= len - 8)
-        {
-          char *newdest = (char *)realloc (dest, len + 16);
-          if (!newdest)
-            return NULL;
-          dest = newdest;
-          len += 16;
-          d = dest + off;
-          *d = 0;
-          end = dest + len;
-        }
       switch (*wstr)
         {
         case 34: // '"'
-          SAFE_APPEND (d, end, "&quot;");
+          APPEND_GROW (d, dest, end, len, "&quot;");
           break;
         case 39: // '\''
-          SAFE_APPEND (d, end, "&#39;");
+          APPEND_GROW (d, dest, end, len, "&#39;");
           break;
         case 38: // '&'
-          SAFE_APPEND (d, end, "&amp;");
+          APPEND_GROW (d, dest, end, len, "&amp;");
           break;
         case 60: // '<'
-          SAFE_APPEND (d, end, "&lt;");
+          APPEND_GROW (d, dest, end, len, "&lt;");
           break;
         case 62: // '>'
-          SAFE_APPEND (d, end, "&gt;");
+          APPEND_GROW (d, dest, end, len, "&gt;");
           break;
         case 96: // '`'
-          SAFE_APPEND (d, end, "&#96;");
+          APPEND_GROW (d, dest, end, len, "&#96;");
           break;
         case 123: // '{'
-          SAFE_APPEND (d, end, "&#123;");
+          APPEND_GROW (d, dest, end, len, "&#123;");
           break;
         case 125: // '}'
-          SAFE_APPEND (d, end, "&#125;");
+          APPEND_GROW (d, dest, end, len, "&#125;");
           break;
         default:
+          if (end - d <= 16)
+            {
+              const int _off = d - dest;
+              char *_new = (char *)realloc (dest, len + 16);
+              if (!_new)
+                return NULL;
+              dest = _new;
+              len += 16;
+              d = dest + _off;
+              end = dest + len;
+            }
           if (*wstr >= 127 || *wstr < 20) // utf8 encodings
             {
               if (!d)
@@ -188,9 +197,8 @@ htmlwescape (BITCODE_TU wstr)
         }
       wstr++;
     }
-done:
   *d = 0;
   return dest;
 }
 
-#undef SAFE_APPEND
+#undef APPEND_GROW
