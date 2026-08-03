e405fcff2eaff86b8389222b7e99529284e7ea0a
Saddam <saddamr3e@gmail.com>
2026-08-03 20:47:53 +0530

    terminate the book_name copy in dxf_CMC and dxfb_CMC

--- src/out_dxf.c
+++ src/out_dxf.c
@@ -1289,7 +1289,10 @@ dxf_CMC (Bit_Chain *restrict dat, Dwg_Color *restrict color, const int dxf,
             {
               char *u8 = bit_convert_TU ((BITCODE_TU)color->book_name);
               if (u8)
-                strncpy (name, u8, 127);
+                {
+                  strncpy (name, u8, 127);
+                  name[127] = '\0';
+                }
               else
                 name[0] = '\0';
               free (u8);
@@ -1305,6 +1308,7 @@ dxf_CMC (Bit_Chain *restrict dat, Dwg_Color *restrict color, const int dxf,
           else
             {
               strncpy (name, color->book_name, 127);
+              name[127] = '\0';
               if (color->name)
                 {
                   strcat (name, "$");
