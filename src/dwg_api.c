dbaa9b36667d11bb435c4bc305168ed39fc0e3d4
Saddam <saddamr3e@gmail.com>
2026-08-13 03:35:27 +0530

    cap sscanf field width in dwg_add_PDFUNDERLAY

--- src/dwg_api.c
+++ src/dwg_api.c
@@ -30905,7 +30905,7 @@ dwg_add_PDFUNDERLAY (Dwg_Object_BLOCK_HEADER *restrict blkhdr,
                     break;
                   }
                 // same base: i++ and inc name
-                sscanf (text, "%s - %d", text1, &i1);
+                sscanf (text, "%79s - %d", text1, &i1);
                 if (strEQ (text1, base))
                   {
                     i++;
